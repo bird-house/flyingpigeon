5d123d552020b91878ebb5e5f0858106b2265fb0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-04 19:06:17 +0100

    update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 if [ -f /etc/debian_version ] ; then
-    sudo apt-get -y --force-yes install wget build-essential gfortran
+    echo "Installing Debian/Ubuntu packages ..."
+    sudo apt-get -y install gfortran
 elif [ -f /etc/redhat-release ] ; then
-    sudo rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    sudo yum -y install wget gcc-c++ gcc-gfortran
+    echo "Installing RedHat/CentOS packages ..."
+    sudo yum -y install gcc-c++ gcc-gfortran
 elif [ `uname -s` = "Darwin" ] ; then
-    brew install wget
+    echo "Installing MacOSX/Homebrew packages ..."
+    #brew install wget
 fi
