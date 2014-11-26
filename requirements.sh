8181786a71d834508acf5a24a8943c784b0619de
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-26 14:07:58 +0100

    nco added to requirements

--- requirements.sh
+++ requirements.sh
@@ -3,9 +3,11 @@
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
     sudo apt-get -y install gfortran
+    sudo apt-get -y install nco
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     sudo yum -y install gcc-c++ gcc-gfortran
+    sudo yum -y install nco
 elif [ `uname -s` = "Darwin" ] ; then
     echo "Installing MacOSX/Homebrew packages ..."
     brew install gcc R
