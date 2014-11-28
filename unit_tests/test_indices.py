9f82f139cdaf956685979a4e2e16cc6e3144c501
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-28 13:27:29 +0100

    test_indices is working

--- unit_tests/test_indices.py
+++ unit_tests/test_indices.py
@@ -10,21 +10,21 @@ def setup():
     pass
 
 @attr('online')   
-def test_icclim():
+def test_indices():
     result = wpsclient.execute(
         service = base.SERVICE,
         identifier = "indice",
         inputs = [('netcdf_file', 'http://localhost:8090/wpscache/tasmax_WAS-44_MPI-M-MPI-ESM-LR_historical_r1i1p1_MPI-CSC-REMO2009_v1_day_20010101-20051231.nc'),
         ('SU','True'),
         ],
-        outputs = [('output', True)],
+        outputs = [('logout', True), ('ncout', True)],
         verbose=False,
         sleep_secs=2
         )
 
     nose.tools.ok_(result['status'] == 'ProcessSucceeded', result)
-    nose.tools.ok_(len(result['processOutputs']) == 3, result)
-    nose.tools.ok_(False, result)
+    nose.tools.ok_(len(result['processOutputs']) == 2, result)
+    #nose.tools.ok_(False, result)
 
     #nose.tools.ok_('txt' in result[0]['reference'], result)
     #content = urllib.urlopen(result[0]['reference']).read()
