6ec3391234b62a6713ece1a68695f7ba5802d444
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-28 11:33:48 +0100

    test_indices.py added

--- unit_tests/test_flyingpigeon.py
+++ unit_tests/test_flyingpigeon.py
@@ -13,23 +13,6 @@ import urllib
 def setup():
     pass
 
-@attr('online')   
-def test_icclim():
-    raise SkipTest
-    result = wpsclient.execute(
-        service = base.SERVICE,
-        identifier = "icclim",
-        inputs = [('file_identifier', 'http://localhost:8090/thredds/fileServer/test/nils.hempelmann@hzg.de/tasmax_day_MPI-ESM-LR_historical_r1i1p1_20040101-20051231.nc'),
-        ('SU','True'),
-        ], #http://localhost:8090/thredds/fileServer/test/nils.hempelmann_hzg.de/tasmax_EUR11_test-pywpsInputbtel_q.nc
-        outputs = [('output', True)],
-        verbose=False
-        )
-
-    nose.tools.ok_('txt' in result[0]['reference'], result)
-    content = urllib.urlopen(result[0]['reference']).read()
-    nose.tools.ok_(not ('failed' in content), content)
-
 @attr('online')
 def test_visualisation():
     raise SkipTest
@@ -43,23 +26,6 @@ def test_visualisation():
     ) 
 
     nose.tools.ok_('html' in result, result)
-
-@attr('online')
-def test_icclim():
-    raise SkipTest
-    result = wpsclient.execute(
-        service = base.SERVICE,
-        identifier = "de.csc.icclim.worker",
-        inputs = [('file_identifier', 'http://localhost:8090/thredds/fileServer/test/nils.hempelmann@hzg.de/tasmax_day_MPI-ESM-LR_historical_r1i1p1_20040101-20051231.nc'),
-        ('SU','True'),
-        ('token', TEST_TOKEN)], #http://localhost:8090/thredds/fileServer/test/nils.hempelmann_hzg.de/tasmax_EUR11_test-pywpsInputbtel_q.nc
-        outputs = [('output', True)],
-        verbose=False
-        )
-
-    nose.tools.ok_('txt' in result[0]['reference'], result)
-    content = urllib.urlopen(result[0]['reference']).read()
-    nose.tools.ok_(not ('failed' in content), content)
 
 @attr('online')
 def test_extractpoints():
