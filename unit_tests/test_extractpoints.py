455294a9c1f852a17a68578e56aaa29b0161e916
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-03 10:28:52 +0100

    fixed extractpoints test

--- unit_tests/test_extractpoints.py
+++ unit_tests/test_extractpoints.py
@@ -10,18 +10,19 @@ def setup():
     pass
 
 @attr('online')   
-def test_indices():
+def test_extractpoints():
     result = wpsclient.execute(
         service = SERVICE,
         identifier = "extractpoints",
         inputs = [('netcdf_file', TESTDATA['tas_EUR-11_ICHEC-EC-EARTH_historical_r1i1p1_KNMI-RACMO22E_v1_mon_200101-200512.nc']),
-          ('coords',['2.356138, 48.846450']), 
-          ('type_nc', True), 
-          ('type_csv', True)],
+          ('coords','2.356138, 48.846450'),
+          #('coords','3.356138, 49.846450'), 2nd point
+          ('type_nc', 'True'), 
+          ('type_csv', 'True')],
         outputs = [('tarout', True)],
         verbose = False,
         sleep_secs=2
         )
     nose.tools.ok_(result['status'] == 'ProcessSucceeded', result)
-    nose.tools.ok_(len(result['processOutputs']) == 2, result)
+    nose.tools.ok_(len(result['processOutputs']) == 1, result)
     
