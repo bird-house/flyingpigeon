3d0c4bfca415e4dac4bd7c1e231b4790c2cace64
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-17 11:28:53 +0100

    missing indices added

--- flyingpigeon/indices.py
+++ flyingpigeon/indices.py
@@ -7,8 +7,34 @@ from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
 _INDICES_ = dict(
-    SU=dict(variable='tasmax', description='Nr of summer days (tasmax as input files)'),
     TG=dict(variable='tas', description='Mean of mean temperatur (tas as input files)'),
+    TX=dict(variable='tasmax', description='Mean of max temperatur (tasmax as input files)'),
+    TN=dict(variable='tasmin', description='Mean of daily min temperatur (tasmin as input files)'),
+    TXn=dict(variable='tasmax', description='Min of daily min temperatur (tasmax as input files)'),
+    TXx=dict(variable='tasmax', description='Max of daily max temperatur (tasmax as input files)'),
+    TNn=dict(variable='tasmin', description='Min of daily min temperatur (tasmin as input files)'),
+    TNx=dict(variable='tasmin', description='Max of daily min temperatur (tasmin as input files)'),
+    SU=dict(variable='tasmax', description='Nr of summer days (tasmax as input files)'),
+    CSU=dict(variable='tasmax', description='Nr of consecutive summer days (tasmax as input files)'),
+    FD=dict(variable='tasmin', description='Nr of frost days (tasmin as input files)'),
+    CFD=dict(variable='tasmin', description='Nr of consecutive frost days (tasmin as input files)'),
+    TR=dict(variable='tasmin', description='... (tasmin as input files)'),
+    ID=dict(variable='tasmax', description='Nr of Ice days (tasmax as input files)'),
+    HD17=dict(variable='tas', description='Heating degree days [sum of 17 degrees - mean temperature] (tas as input files)'),
+    GD4=dict(variable='tas', description='Growing degree days [sum of TG > 4 degrees] (tas as input files)'),
+    RR=dict(variable='pr', description='Precipitation flux mean (mon / year) (pr as input files)'),
+    RR1=dict(variable='pr', description='Nr of days with precipitation > 1 mm  (pr as input files)'),
+    CWD=dict(variable='pr', description='Consecutive wet days (pr as input files)'),
+    CDD=dict(variable='pr', description='Consecutive dry days (pr as input files)'),
+    SDII=dict(variable='pr', description='Simple daily intensity index for wet days [mm/wet day] (pr as input files)'),
+    R10mm=dict(variable='pr', description='Nr of days >10mm (pr as input files)'),
+    R20mm=dict(variable='pr', description='Nr of days with precipitation > 20 mm (pr as input files)'),
+    RX1day=dict(variable='pr', description='Highest 1-day precipitation amount (pr as input files)'),
+    RX5day=dict(variable='pr', description='Highest 5-day precipitation amount (pr as input files)'),
+    SD=dict(variable='prsn', description='Nr of snow days (prsn as input files)'),
+    SD1=dict(variable='prsn', description='Nr of days with snow > 1cm  (prsn as input files)'),
+    SD5cm=dict(variable='prsn', description='Nr of days with snow > 5cm (prsn as input files)'),
+    SD50cm=dict(variable='prsn', description='Nr of days with snow > 50 cm (prsn as input files)'),
 )
 
 def indices():
