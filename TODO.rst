60cbdec7670c044252605b0a26e3f3dbf7c79588
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-22 11:23:24 +0100

    ocgis test for eur11-day added

--- TODO.rst
+++ TODO.rst
@@ -1,6 +1,7 @@
 Todo List
 *********
 
+* use common module for status logging
 * configure num CPUs which can be used
 * check process timeout configuration (dispel, malleefowl workflow, wps ...)
 * use multiprocessing for processes.
--- unit_tests/test_ocgis.py
+++ unit_tests/test_ocgis.py
@@ -3,10 +3,14 @@ from unittest import TestCase
 from nose import SkipTest
 from nose.plugins.attrib import attr
 
+import ocgis
+
 from __init__ import TESTDATA, SERVICE
 
 import tempfile
 from netCDF4 import Dataset
+
+from flyingpigeon.utils import local_path
 
 class OCGISTestCase(TestCase):
 
@@ -17,28 +21,29 @@ class OCGISTestCase(TestCase):
         # TODO: ocgis does not like file:// urls
         from urllib2 import urlparse
         # tas
-        url_parts = urlparse.urlparse(
+        cls.tas_nc = local_path(
             TESTDATA['tas_EUR-11_ICHEC-EC-EARTH_rcp45_r1i1p1_KNMI-RACMO22E_v1_mon_200601-201012.nc'])
-        cls.tas_nc = url_parts.path
         # tasmax
-        url_parts = urlparse.urlparse(
+        cls.tasmax_nc = local_path(
             TESTDATA['tasmax_WAS-44_MPI-M-MPI-ESM-LR_historical_r1i1p1_MPI-CSC-REMO2009_v1_day_20010101-20051231.nc'])
-        cls.tasmax_nc = url_parts.path
+        # eur-11, day
+        cls.tasmax_eur11_day_2006_nc = local_path(
+            TESTDATA['tasmax_EUR-11_ICHEC-EC-EARTH_rcp85_r1i1p1_KNMI-RACMO22E_v1_day_20060101-20101231.nc'])
 
     @attr('testdata')
     def test_ocgis_inspect(self):
-        import ocgis
         rd = ocgis.RequestDataset(self.tas_nc, 'tas')
         rd.inspect()
 
     @attr('testdata')
+    @attr('slow')
     def test_ocgis_su_tasmax(self):
-        import ocgis
+        raise SkipTest
         out_dir = tempfile.mkdtemp()
         prefix = 'tasmax_su'
         
         calc_icclim = [{'func':'icclim_SU','name':'SU'}]
-        rd = ocgis.RequestDataset(self.tasmax_nc, "tasmax") # TODO: time_range=[dt1, dt2]
+        rd = ocgis.RequestDataset(self.tasmax_nc, "tasmax") 
         SU_file = ocgis.OcgOperations(
             dataset=rd,
             calc=calc_icclim,
@@ -54,6 +59,31 @@ class OCGISTestCase(TestCase):
         nose.tools.ok_('SU' in result.variables, result.variables.keys())
         # 5 years
         nose.tools.ok_(len(result.variables['time']) == 5, len(result.variables['time']))
+
+    @attr('testdata')
+    @attr('slow')
+    def test_ocgis_eur11_day(self):
+        #raise SkipTest
+        out_dir = tempfile.mkdtemp()
+        prefix = 'tasmax_su'
+        
+        calc_icclim = [{'func':'icclim_SU','name':'SU'}]
+        rd = ocgis.RequestDataset(self.tasmax_eur11_day_2006_nc, "tasmax", time_region = {'year':[2006]}) 
+        SU_file = ocgis.OcgOperations(
+            dataset=rd,
+            calc=calc_icclim,
+            calc_grouping=['year'],
+            prefix=prefix,
+            output_format=self.output_format,
+            dir_output=out_dir,
+            add_auxiliary_files=False).execute()
+
+        from os.path import join
+        result = Dataset(join(out_dir, prefix + '.' + self.output_format))
+        # SU variable must be in result
+        nose.tools.ok_('SU' in result.variables, result.variables.keys())
+        # 5 years
+        nose.tools.ok_(len(result.variables['time']) == 1, len(result.variables['time']))
         
         
     
