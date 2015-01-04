04d51c233eafd5eb379765462c295577e5a9ac84
Nils Hempelmann <info@nilshempelmann.de>
2015-01-05 00:07:33 +0100

    concatination

--- flyingpigeon/standalone/cordexviewer.py
+++ flyingpigeon/standalone/cordexviewer.py
@@ -31,6 +31,7 @@ def file_sorter(ncs):
 try:
   data_basedir = '/media/nils/Iomega HDD/data/'
   normalized_dir = '/media/nils/Iomega HDD/data/normalized/'
+  concat_dir = '/media/nils/Iomega HDD/data/concat/'
   polygon_dir = '/media/nils/Iomega HDD/data/polygon/'
   icclim = '/media/nils/Iomega HDD/data/icclim'
   # logger.debug('starting cv_creator')
@@ -51,89 +52,127 @@ try:
   sci = ShpCabinetIterator(geoms)
 
   # ref_time = [datetime(1971,01,01),datetime(2000,12,31)]
-  ncs = [os.path.join(icclim, f) for f in os.listdir(icclim)]
-  print 'preparation Workdir for clipping done!'
-  print '%i files found in DATA_DIR' % (len(ncs))
+  #ncs = [os.path.join(icclim, f) for f in os.listdir(icclim)]
+  #print 'preparation Workdir for clipping done!'
+  #print '%i files found in DATA_DIR' % (len(ncs))
   
-  exp = file_sorter(ncs)
-  print 'Dictionary build with %i Experiments' % (len(exp.keys()))
+  #exp = file_sorter(ncs)
+  #print 'Dictionary build with %i Experiments' % (len(exp.keys()))
 #  print exp.keys()
 
 except Exception as e:
   msg = 'Clipping preparation failed: %s ' % (e)
   print msg
   
-c = 0  
-for key in exp.keys():
-  try:
-    c = c + 1
-    ncs = exp[key]
-    # ncs = get_sorted_uris_by_time_dimension(nc)
-    ncs.sort()
-    var = key.split('_')[0]
-    scenario = key.split('_')[3]
-    rd = ocgis.RequestDataset(ncs, var) # 
-    time_range=[datetime(1971,01,01) , datetime(2000,12,31)]
+#c = 0  
+#for key in exp.keys():
+  #try:
+    #c = c + 1
+    #ncs = exp[key]
+    ## ncs = get_sorted_uris_by_time_dimension(nc)
+    #ncs.sort()
+    #var = key.split('_')[0]
+    #scenario = key.split('_')[3]
+    #rd = ocgis.RequestDataset(ncs, var) # 
+    #time_range=[datetime(1971,01,01) , datetime(2000,12,31)]
     
-    print 'preperaiton for %s' % (key)
-  except Exception as e:
-    msg = 'sorting preparation failed for : %s %s ' % ( key , e)
-    # logger.error(msg)
-    # outlog = outlog + msg + '\n'
-    print msg
+    #print 'preperaiton for %s' % (key)
+  #except Exception as e:
+    #msg = 'sorting preparation failed for : %s %s ' % ( key , e)
+    ## logger.error(msg)
+    ## outlog = outlog + msg + '\n'
+    #print msg
   
-  for land in europa:
-    try:
-      select_ugid = []
-      geom_rows = []
-      for row in sci:
-        if row['properties']['adm0_a3'] == land:
-          select_ugid.append(row['properties']['UGID'])
-          geom_rows.append(row)
-          
-      # select_ugid.sort()
-      if not os.path.exists(os.path.join(polygon_dir, var , scenario, land)):
-        os.makedirs(os.path.join(polygon_dir, var , scenario, land))
-      OUT_DIR = os.path.join(polygon_dir, var , scenario, land)
-      
-      # dir_output = tempfile.mkdtemp()
-      ocgis.env.DIR_OUTPUT = OUT_DIR
-      prefix = key.replace('EUR',land).strip('_mon')
-      print 'done for %s ' % (prefix)
+  #if not os.path.exists(os.path.join(concat_dir)):
+    #os.makedirs(os.path.join(concat_dir))
+  #OUT_DIR = os.path.join(concat_dir)
+  
+  #try: 
+    #ocgis.env.DIR_OUTPUT = OUT_DIR
+    #prefix = key
     
-    except Exception as e:
-      msg = 'processing failed for file  : %s ' % ( e)
-      print msg 
+    #if not os.path.exists(os.path.join(OUT_DIR,('%s.nc' % (prefix)))):
+      #geom_nc = ocgis.OcgOperations(dataset=rd, dir_output=OUT_DIR, output_format='nc', prefix=prefix, add_auxiliary_files=False ).execute()
+      ## print 'concatination done for %s ' % (prefix)
+    #else : 
+      #print 'allready concatinated for %s ' % (prefix)
+    
+  #except Exception as e:
+    #msg = '!!! concatination failed for : %s %s ' % ( prefix , e)
+    ## logger.error(msg)
+    ## outlog = outlog + msg + '\n'
+    #print msg
+
+ncs = [os.path.join(concat_dir, f) for f in os.listdir(concat_dir)]
+
+for nc in ncs:  
+  try:
+    for land in europa:
+      try: 
+        p, f = os.path.split(nc)
+        var = f.split('_')[0]
+        scenario = f.split('_')[3]
+        rd = ocgis.RequestDataset(nc, var) # 
+        time_range=[datetime(1971,01,01) , datetime(2000,12,31)]
+        select_ugid = []
+        geom_rows = []
+        for row in sci:
+          if row['properties']['adm0_a3'] == land:
+            select_ugid.append(row['properties']['UGID'])
+            geom_rows.append(row)
+            
+        # select_ugid.sort()
+        if not os.path.exists(os.path.join(polygon_dir, var , scenario, land)):
+          os.makedirs(os.path.join(polygon_dir, var , scenario, land))
+        OUT_DIR = os.path.join(polygon_dir, var, scenario, land)
+
+        if var == 'RR' or var == 'R20mm' or var == 'SU' or var ==  'ID': # CDD  CSU  TX  TXx
+          calc = [{'func':'sum', 'name':var}] 
+          calc_grouping = ['year']
+          
+        elif var == 'TXx' or var == 'RX5day':
+          calc = [{'func':'max', 'name':var}] 
+          calc_grouping = ['year']
+        else :
+          calc = [{'func':'mean', 'name':var}] 
+          calc_grouping = ['year']
+        
+        if calc_grouping[0] == 'year':
+          prefix = f.replace('EUR',land).replace('_mon','_yr').strip('.nc')
+        
+        # dir_output = tempfile.mkdtemp()
+        ocgis.env.DIR_OUTPUT = OUT_DIR
+        
+        
+        geom_nc = ocgis.OcgOperations(dataset=rd, dir_output=OUT_DIR, geom=geoms, select_ugid=select_ugid,  output_format='nc', prefix=prefix, add_auxiliary_files=False ).execute()
+        print 'done for %s ' % (prefix)
+      except Exception as e:
+        msg = 'clipping filed for : %s %s ' % (land, e)
+        print msg
+  except Exception as e:
+    msg = 'processing failed for file  : %s %s ' % (nc, e)
+    print msg 
       
-    try:
-      # make temofiles 
+    #try:
+      ## make temofiles 
       
-      if var == 'RR' or var == 'R20mm' or var == 'SU': # CDD  CSU  ID  TX  TXx
-        calc = [{'func':'sum'}] 
-        calc_grouping = ['year']
-      elif var == 'TXx' or var == 'RX5day':
-        calc = [{'func':'max'}] 
-        calc_grouping = ['year']
-      else :
-        calc = [{'func':'mean'}] 
-        calc_grouping = ['year']
               
-      #temp_id1, nc = tempfile.mkstemp()
-      #p, temp_nc = os.path.split(nc)
-      #temp_id2, ref = tempfile.mkstemp()
-      #p, temp_ref = os.path.split(ref)
+      ##temp_id1, nc = tempfile.mkstemp()
+      ##p, temp_nc = os.path.split(nc)
+      ##temp_id2, ref = tempfile.mkstemp()
+      ##p, temp_ref = os.path.split(ref)
 
-      geom_nc = ocgis.OcgOperations(dataset=rd, geom=geoms, dir_output=OUT_DIR, output_format='nc', select_ugid=select_ugid, prefix=prefix, add_auxiliary_files=False ).execute()
+      #geom_nc = ocgis.OcgOperations(dataset=rd, geom=geoms, dir_output=OUT_DIR, output_format='nc', select_ugid=select_ugid, prefix=prefix, add_auxiliary_files=False ).execute()
 
-      geom_ref = ocgis.OcgOperations(dataset=rd, geom=geoms, dir_output=OUT_DIR, output_format='nc', select_ugid=select_ugid, prefix=prefix, add_auxiliary_files=False, calc=calc, calc_grouping=calc_grouping , time_range=time_range  ).execute()
+      #geom_ref = ocgis.OcgOperations(dataset=rd, geom=geoms, dir_output=OUT_DIR, output_format='nc', select_ugid=select_ugid, prefix=prefix, add_auxiliary_files=False, calc=calc, calc_grouping=calc_grouping , time_range=time_range  ).execute()
       
-      #os.remove(geom_nc)
-      #os.remove(geom_ref)
+      ##os.remove(geom_nc)
+      ##os.remove(geom_ref)
       
-      print 'normalized fieldmean for key %s done! ' % ( key )
-    except Exception as e:
-      msg = 'normalized fieldmean failed for key  : %s %s ' % ( key , e)
-      print msg
+      #print 'normalized fieldmean for key %s done! ' % ( key )
+    #except Exception as e:
+      #msg = 'normalized fieldmean failed for key  : %s %s ' % ( key , e)
+      #print msg
       
       
       #outlog = outlog + ('calculation of polygon %s with variable %s ... done \n'% (prefix , var))
