c3b2f8b9678a70b5beda2d4d6eb5bc9f4f898589
Nils Hempelmann <info@nilshempelmann.de>
2015-03-16 20:17:36 +0100

    returntimes

--- flyingpigeon/extremevents.py
+++ flyingpigeon/extremevents.py
@@ -1,13 +1,8 @@
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
-def returnlevels(ts):
-    from numpy import vstack, array
-    import rpy2.robjects.numpy2ri
-    rpy2.robjects.numpy2ri.activate()
-    evd = importr('evd')
-    
-    def RL(T,a,b,s):
+def RL(T,a,b,s):
+        T = float(T)
         from math import log 
         yT = -1/log(1 - 1/T)
         if(s != 0):
@@ -16,7 +11,15 @@ def returnlevels(ts):
             zT= a + b * log(yT)
         return(zT)
 
-    ts = array(ts)
+def returnlevels(ts):
+    from numpy import vstack, array, percentile
+    import rpy2.robjects.numpy2ri
+    from rpy2.robjects import FloatVector
+    
+    rpy2.robjects.numpy2ri.activate()
+    evd = importr('evd')
+    
+    ts = FloatVector(ts)
     RR=evd.fgev(ts)
     a=RR.rx2('estimate')[0] # RR.fgev$estimate[1]
     b=RR.rx2('estimate')[1] # RR.fgev$estimate[2]
@@ -24,14 +27,54 @@ def returnlevels(ts):
     
     rp = [ 5. ,10. ,20., 50. ,100. ,200., 300., 400. ,500. ,1000. ]
     rl = []
-    # rl_min = []
-    # rl_max = []
-    # T=1000.
+    per10 = []
+    per90 = []
+
     for T in rp:
         rl.append(RL(T,a,b,s))
-    rl_c = vstack((rp,rl)) 
+
+##        RL_bt = rl_bootstrap(ts, T)
+##        per = percentile(RL_bt,[10,90])
+##        per10.append(per[0])
+##        per90.append(per[1])
+
+    rl_c = vstack((rp,rl))
+    
     return (rl_c)
 
+def rl_bootstrap(ts, T):
+    import rpy2.robjects.numpy2ri
+    from rpy2.robjects import FloatVector
+    from numpy import sort , array
+    rpy2.robjects.numpy2ri.activate()
+    rsample = robjects.globalenv.get("sample")
+    runique = robjects.globalenv.get("unique")
+    evd = importr('evd')
+
+    ts = FloatVector(ts)
+    II= range(0,len(ts),1)
+
+    ts_fgev = (evd.fgev(ts))
+    a=ts_fgev.rx2('estimate')[0]
+    b=ts_fgev.rx2('estimate')[1] 
+    s=ts_fgev.rx2('estimate')[2]
+
+    RL0 = RL(T,a,b,s)
+    nsim=100
+    RL_bt=[]
+    for i in range(0,nsim,1):
+        II_s= sort(runique(rsample(II,replace=True)))
+        ts_s = array([ts[i] for i in II_s])
+        ts_s_fgev=evd.fgev(ts_s)
+        a=ts_s_fgev.rx2('estimate')[0] # RR.fgev$estimate[1]
+        b=ts_s_fgev.rx2('estimate')[1] # RR.fgev$estimate[2]
+        s=ts_s_fgev.rx2('estimate')[2] # fgev$estimate[3]
+        RL_bt.append(RL(T,a,b,s))
+        
+    return RL_bt
+
+    
+    
 
 #def returnlevel(ts):
 #  from numpy import vstack
