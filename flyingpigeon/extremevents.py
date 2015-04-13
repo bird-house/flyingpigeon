c4023dba17c310ac654961daff5677093107e6ea
Nils Hempelmann <info@nilshempelmann.de>
2015-04-13 19:07:43 +0200

    extremevents with uncertainties

--- flyingpigeon/extremevents.py
+++ flyingpigeon/extremevents.py
@@ -11,41 +11,11 @@ def RL(T,a,b,s):
             zT= a + b * log(yT)
         return(zT)
 
-def returnlevels(ts):
-    from numpy import vstack, array, percentile
-    import rpy2.robjects.numpy2ri
-    from rpy2.robjects import FloatVector
-    
-    rpy2.robjects.numpy2ri.activate()
-    evd = importr('evd')
-    
-    ts = FloatVector(ts)
-    RR=evd.fgev(ts)
-    a=RR.rx2('estimate')[0] # RR.fgev$estimate[1]
-    b=RR.rx2('estimate')[1] # RR.fgev$estimate[2]
-    s=RR.rx2('estimate')[2] # fgev$estimate[3]
-    
-    rp = [ 5. ,10. ,20., 50. ,100. ,200., 300., 400. ,500. ,1000. ]
-    rl = []
-    per10 = []
-    per90 = []
-
-    for T in rp:
-        rl.append(RL(T,a,b,s))
-
-##        RL_bt = rl_bootstrap(ts, T)
-##        per = percentile(RL_bt,[10,90])
-##        per10.append(per[0])
-##        per90.append(per[1])
-
-    rl_c = vstack((rp,rl))
-    
-    return (rl_c)
-
-def rl_bootstrap(ts, T):
+def rl_bootstrap(ts, T=100, nsim=1000):
     import rpy2.robjects.numpy2ri
     from rpy2.robjects import FloatVector
     from numpy import sort , array
+    
     rpy2.robjects.numpy2ri.activate()
     rsample = robjects.globalenv.get("sample")
     runique = robjects.globalenv.get("unique")
@@ -60,7 +30,6 @@ def rl_bootstrap(ts, T):
     s=ts_fgev.rx2('estimate')[2]
 
     RL0 = RL(T,a,b,s)
-    nsim=100
     RL_bt=[]
     for i in range(0,nsim,1):
         II_s= sort(runique(rsample(II,replace=True)))
@@ -73,35 +42,35 @@ def rl_bootstrap(ts, T):
         
     return RL_bt
 
+def eventdistribution(ts, nsim=100, rp = [ 5. ,10. ,20., 50. ,100. ,200., 300., 400. ,500., 700, 1000. ]):
+    from rpy2.robjects import FloatVector
+    from numpy import vstack, array, percentile
+    import rpy2.robjects.numpy2ri
     
+    ts = FloatVector(ts)    
+    rpy2.robjects.numpy2ri.activate()
+    evd = importr('evd')
     
-
-#def returnlevel(ts):
-#  from numpy import vstack
-
-#  evir = importr('evir')
-#  base = importr('base')
-#  gr = importr('grDevices')
-
-  
-#  rp = [ 5 ,10 ,20, 50 ,100 ,200, 300, 400 ,500 ,1000 ]
-#  rl = []
-#  rl_min = []
-#  rl_max = []
-  
-#  xx = evir.gev(ts)
-#  for r in rp:
-        #gr.dev_new()
-#        y= evir.rlevel_gev(xx , r )#, add = True )
+    RR = evd.fgev(ts)
+    a=RR.rx2('estimate')[0] # RR.fgev$estimate[1]
+    b=RR.rx2('estimate')[1] # RR.fgev$estimate[2]
+    s=RR.rx2('estimate')[2] # fgev$estimate[3]
     
-#        rl_min.append(y[0])
-#        rl.append(y[1])
-#        rl_max.append(y[2])
+    rl = []
+    edist = []
+
+    per10 = []
+    per90 = []
+
+    for T in rp:
+        rl.append(RL(T,a,b,s))
+        
+        RL_bt = rl_bootstrap(ts,T, nsim=100)
+        per = percentile(RL_bt,[10,90])
+        per10.append(per[0])
+        per90.append(per[1])
+
+    rl_c = vstack((rp,per10,rl, per90))
     
-#  rl_c = vstack((rp, rl_min, rl, rl_max))
-#  return rl_c
+    return (rl_c)
 
-
-
-
-   
