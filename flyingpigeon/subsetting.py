ef5061c94eb1e78f3dbc3316bd7ce714bec96cc3
Nils Hempelmann <info@nilshempelmann.de>
2015-05-27 23:58:12 +0200

    merge

--- flyingpigeon/subsetting.py
+++ flyingpigeon/subsetting.py
@@ -37,3 +37,48 @@ def masking(resource , mask, prefix=None, dir_output=None):
   system(call)
   
   return resource_masked
+
+
+def get_dimension_map(resource): 
+  """ returns the dimension map for a file, required for ocgis processing. 
+  file must have a DRS conform filename (see: utils.drs_filename())
+  
+  :param resource: str input file path
+  """
+
+  from os.path import basename
+  file_name = basename(resource)
+  
+  dim_map1 = {'X': {'variable': 'lon', 'dimension': 'x', 'pos': 2},
+              'Y': {'variable': 'lat', 'dimension': 'y', 'pos': 1},
+              'T': {'variable': 'time', 'dimension': 'time', 'pos': 0}}
+  
+  #dim_map2 = {'X': {'variable': 'lon', 'dimension': 'x', 'pos': 2},
+              #'Y': {'variable': 'lat', 'dimension': 'y', 'pos': 1},
+              #'T': {'variable': 'time', 'dimension': 'time', 'pos': 0, 'bounds': 'time_bnds'}}
+  
+  dim_map3 = {'X': {'variable': 'rlon', 'dimension': 'x', 'pos': 2},
+              'Y': {'variable': 'rlat', 'dimension': 'y', 'pos': 1},
+              'T': {'variable': 'time', 'dimension': 'time', 'pos': 0 }}
+
+  dim_map4 = {'X': {'variable': 'Actual_longitude', 'dimension': 'x', 'pos': 2},
+              'Y': {'variable': 'Actual_latitude', 'dimension': 'y', 'pos': 1},
+              'T': {'variable': 'time', 'dimension': 'time', 'pos': 0 }}
+
+  
+  if 'CM5A-MR_WRF331F' in file_name: 
+    dimension_map = dim_map1
+  elif 'CNRM-CM5_CNRM-ALADIN53' in file_name: 
+    dimension_map = dim_map1
+  elif 'MPI-ESM-LR_REMO019' in file_name: 
+    dimension_map = dim_map1
+  elif 'CLMcom-CCLM4-8-17' in file_name:
+    dimension_map = dim_map1
+  elif '_v11.0.nc'  in file_name: # EOBS Data
+    dimension_map = dim_map4
+  #elif 'KNMI-RACMO22E' in file_name:   
+    #dimension_map = dim_map1
+  else:     
+    dimension_map = None
+    
+  return dimension_map
