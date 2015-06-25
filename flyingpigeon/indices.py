9af86ed758c36000f7a4adf7e90feb529c3179d6
Nils Hempelmann <info@nilshempelmann.de>
2015-06-25 17:55:15 +0200

    indices update

--- flyingpigeon/indices.py
+++ flyingpigeon/indices.py
@@ -3,7 +3,6 @@ from cdo import Cdo
 import tempfile
 
 from .exceptions import CalculationException
-
 from malleefowl import wpslogging as logging
 #import logging
 logger = logging.getLogger(__name__)
@@ -21,23 +20,23 @@ _INDICES_ = dict(
     CSU=dict(variable='tasmax', description='Nr of consecutive summer days (tasmax as input files)'),
     FD=dict(variable='tasmin', description='Nr of frost days (tasmin as input files)'),
     CFD=dict(variable='tasmin', description='Nr of consecutive frost days (tasmin as input files)'),
-    TR=dict(variable='tasmin', description='Tropical nights - number of days where daily minimum temperature > 20 degrees.(tasmin as input files)'),
+    TR=dict(variable='tasmin', description='Tropical nights - number of days where daily minimum temperature >= 20 degrees.(tasmin as input files)'),
     ID=dict(variable='tasmax', description='Nr of Ice days (tasmax as input files)'),
     HD17=dict(variable='tas', description='Heating degree days [sum of 17 degrees - mean temperature] (tas as input files)'),
-    GD4=dict(variable='tas', description='Growing degree days [sum of TG > 4 degrees] (tas as input files)'),
+    GD4=dict(variable='tas', description='Growing degree days [sum of TG >= 4 degrees] (tas as input files)'),
     RR=dict(variable='pr', description='Precipitation flux mean (mon / year) (pr as input files)'),
     RR1=dict(variable='pr', description='Nr of days with precipitation > 1 mm  (pr as input files)'),
     CWD=dict(variable='pr', description='Consecutive wet days (pr as input files)'),
     CDD=dict(variable='pr', description='Consecutive dry days (pr as input files)'),
     SDII=dict(variable='pr', description='Simple daily intensity index for wet days [mm/wet day] (pr as input files)'),
     R10mm=dict(variable='pr', description='Nr of days >10mm (pr as input files)'),
-    R20mm=dict(variable='pr', description='Nr of days with precipitation > 20 mm (pr as input files)'),
+    R20mm=dict(variable='pr', description='Nr of days with precipitation >= 20 mm (pr as input files)'),
     RX1day=dict(variable='pr', description='Highest 1-day precipitation amount (pr as input files)'),
     RX5day=dict(variable='pr', description='Highest 5-day precipitation amount (pr as input files)'),
     SD=dict(variable='prsn', description='Nr of snow days (prsn as input files)'),
-    SD1=dict(variable='prsn', description='Nr of days with snow > 1cm  (prsn as input files)'),
-    SD5cm=dict(variable='prsn', description='Nr of days with snow > 5cm (prsn as input files)'),
-    SD50cm=dict(variable='prsn', description='Nr of days with snow > 50 cm (prsn as input files)'),
+    SD1=dict(variable='prsn', description='Nr of days with snow >= 1cm  (prsn as input files)'),
+    SD5cm=dict(variable='prsn', description='Nr of days with snow >= 5cm (prsn as input files)'),
+    SD50cm=dict(variable='prsn', description='Nr of days with snow >= 50 cm (prsn as input files)'),
 )
 
 def indices():
@@ -88,11 +87,6 @@ def calc_indice(resource=[], indice="TG", grouping="yr", out_dir=None):
 
     :return: netcdf files with calculated indices
     """
-
-    ## ocgis.env.OVERWRITE = True
-    ## ocgis.env.DIR_DATA = os.path.curdir
-    ## ocgis.env.DIR_OUTPUT = outdir    
-    ## output_crs = None
 
     from utils import aggregations, calc_grouping
 
