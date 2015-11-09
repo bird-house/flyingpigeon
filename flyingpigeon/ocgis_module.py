a7ee89ab40c6812ae1b9b53bf62288207bfae643
Nils Hempelmann <info@nilshempelmann.de>
2015-11-09 10:26:05 +0100

    free memory check in ocgis module

--- flyingpigeon/ocgis_module.py
+++ flyingpigeon/ocgis_module.py
@@ -24,11 +24,19 @@ def call(resource=[], variable=None, dimension_map=None, calc=None,
     resource = list([resource])
 
   # check memory load 
-  limit_memory_mb = 475.0 # # to reduce the load of the memory, calculation is perfored in chunks
+  from flyingpigeon.utils import FreeMemory
+  f = FreeMemory()
+  mem_bytes = f.user_free
+
+  # mem_gib = mem_bytes/(1024.**3)
+
+  limit_memory = mem_bytes * 0.8 # 475.0 # to avoid MemoryError the, calculation is perfored in chunks by big data_input 
   fsize = 0 
   for nc in resource: 
     fsize = fsize + getsize(nc)
   
+  logger.info('data_input = %s ; free_memory: %s ' % (fsize, mem_bytes))
+
   # execute ocgis 
   logger.info('Execute ocgis module call function')
   rd = RequestDataset(resource, variable=variable, dimension_map=dimension_map, conform_units_to=conform_units_to, time_region=time_region)
@@ -41,17 +49,20 @@ def call(resource=[], variable=None, dimension_map=None, calc=None,
         geom=geom,
         add_auxiliary_files=False)
 
-  if fsize / 1000000 <= limit_memory_mb :          
+  if fsize <= limit_memory:  
+    logger.info('ocgis module call as ops.execute() ')        
     geom_file = ops.execute()
 
   else: 
     # calcultion of chunk size
+    logger.info('ocgis module call as compute(ops) ')
     size = ops.get_base_request_size()
     nb_time_coordinates_rd = size['variables'][variable]['temporal']['shape'][0]
     element_in_kb = size['total']/reduce(lambda x,y: x*y,size['variables'][variable]['value']['shape'])
     element_in_mb = element_in_kb*0.001
     tile_dim = sqrt(limit_memory_mb/(element_in_mb*nb_time_coordinates_rd))
+    
     geom_file = compute(ops, tile_dimension=int(tile_dim), verbose=True)        
   
-  logger.info(' Succeeded with ocgis module call function ')
+  logger.info('Succeeded with ocgis module call function')
   return geom_file
