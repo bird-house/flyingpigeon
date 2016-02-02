36cd348b82c513aef7d4b1a54076674355d800bb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-02 11:02:12 +0100

    added caps test

---
+++ tests/test_wps_caps.py
@@ -0,0 +1,14 @@
+import nose.tools
+from tests.common import WpsTestClient
+
+def test_caps():
+    wps = WpsTestClient()
+    resp = wps.get(service='wps', request='getcapabilities')
+    names = resp.xpath_text('/wps:Capabilities'
+                            '/wps:ProcessOfferings'
+                            '/wps:Process'
+                            '/ows:Identifier')
+    sorted_names = sorted(names.split())
+    expected_names = ['analogs', 'ensembleRobustness', 'eobs_to_cordex', 'extractpoints', 'indices_single', 'segetalflora', 'subset_countries', 'visualisation']
+    nose.tools.ok_(sorted_names == expected_names, sorted_names)
+
