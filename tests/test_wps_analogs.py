726da091e733d49986d973d907472e967853d477
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-02 11:05:58 +0100

    enabled analogs test

--- tests/test_wps_analogs.py
+++ tests/test_wps_analogs.py
@@ -6,7 +6,6 @@ from tests.common import WpsTestClient, assert_response_success
 
 @attr('online')
 def test_wps_analogs():
-    raise SkipTest
     wps = WpsTestClient()
     datainputs = "[experiment=NCEP;dateSt=1958-07-15;dateEn=1958-12-31]"
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='analogs',
