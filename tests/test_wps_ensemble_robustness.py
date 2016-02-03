69d60f24250f5a0b16211ca9ce052076b1b7e01c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-03 17:13:23 +0100

    using local testdata for ensemble robustness

--- tests/test_wps_ensemble_robustness.py
+++ tests/test_wps_ensemble_robustness.py
@@ -3,10 +3,9 @@ from nose.plugins.attrib import attr
 
 from tests.common import WpsTestClient, TESTDATA, assert_response_success
 
-@attr('online')
 def test_wps_ensemble_robustness():
     wps = WpsTestClient()
-    datainputs = "[resource={0};start=1980;end=2010]".format(TESTDATA['eurocordex_nc_1'])
+    datainputs = "[resource={0};start=2006;end=2006]".format(TESTDATA['cmip5_tasmax_nc'])
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='ensembleRobustness',
                    datainputs=datainputs)
     assert_response_success(resp)
