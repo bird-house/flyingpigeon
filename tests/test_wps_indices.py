da0435eb001147828691e74e781888792f9f6924
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-03 17:14:56 +0100

    using local testdata for indice test

--- tests/test_wps_indices.py
+++ tests/test_wps_indices.py
@@ -3,10 +3,9 @@ from nose.plugins.attrib import attr
 
 from tests.common import WpsTestClient, TESTDATA, assert_response_success
 
-@attr('online')
 def test_wps_indices_single():
     wps = WpsTestClient()
-    datainputs = "[resource={0};indices=SU]".format(TESTDATA['eurocordex_nc_1'])
+    datainputs = "[resource={0};indices=SU]".format(TESTDATA['cmip5_tasmax_nc'])
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='indices_single',
                    datainputs=datainputs)
     assert_response_success(resp)
