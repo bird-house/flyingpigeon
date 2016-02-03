ea1165c7a6d05ef56b86cc5c3fc14248b6f32c16
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-03 17:16:42 +0100

    speed up eobs test

--- tests/test_wps_eobs_to_cordex.py
+++ tests/test_wps_eobs_to_cordex.py
@@ -8,7 +8,7 @@ from tests.common import WpsTestClient, assert_response_success, TESTDATA
 def test_wps_eobs_to_cordex():
     #raise SkipTest
     wps = WpsTestClient()
-    datainputs = "[var_eobs=tg;start=1950;end=1951]".format(TESTDATA['eurocordex_nc_1'])
+    datainputs = "[var_eobs=tg;start=1950;end=1950]".format(TESTDATA['eurocordex_nc_1'])
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='eobs_to_cordex',
                    datainputs=datainputs)
     assert_response_success(resp)
