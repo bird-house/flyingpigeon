e1821b112062504916b4eb123153ae5b6ecfcf23
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-03 17:10:54 +0100

    using local test data in visualisation test

--- tests/test_wps_visualisation.py
+++ tests/test_wps_visualisation.py
@@ -3,11 +3,9 @@ from nose.plugins.attrib import attr
 
 from tests.common import WpsTestClient, TESTDATA, assert_response_success
 
-@attr('online')
 def test_wps_visualisation():
     wps = WpsTestClient()
-    datainputs = "[resource={0};resource={1};variable=slp]".format(
-        TESTDATA['noaa_slp_1955_nc'], TESTDATA['noaa_slp_1956_nc'])
+    datainputs = "[resource={0};variable=tasmax]".format(TESTDATA['cmip5_tasmax_nc'])
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='visualisation',
                    datainputs=datainputs)
     assert_response_success(resp)
