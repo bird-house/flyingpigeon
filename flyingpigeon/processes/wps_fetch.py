cfa458712a542794c78435fedb4e94748adb0e2c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-18 16:45:33 +0100

    added wps_fetch process

---
+++ flyingpigeon/processes/wps_fetch.py
@@ -0,0 +1,47 @@
+import os
+
+from pywps.Process import WPSProcess
+
+import logging
+logger = logging.getLogger(__name__)
+
+class FetchProcess(WPSProcess):
+    def __init__(self):
+        WPSProcess.__init__(self,
+            identifier="fetch",
+            title="Download Resources",
+            version = "0.1",
+            abstract="This process downloads resources (limited to 50GB) to the local file system and returns a textfile with appropriate pathes",
+            statusSupported=True,
+            storeSupported=True)
+
+        self.resource = self.addComplexInput(
+            identifier="resource",
+            title="NetCDF File",
+            abstract="NetCDF File",
+            minOccurs=1,
+            maxOccurs=100,
+            maxmegabites=5000,
+            formats=[{"mimeType":"application/x-netcdf"}],
+            )
+
+        self.output = self.addComplexOutput(
+            identifier="output",
+            title="Fetched Files",
+            abstract="File containing the local pathes to downloades files",
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
+
+    def execute(self):
+        resources = self.getInputValues(identifier='resource')
+        
+        filename = 'out.txt'
+        with open(filename, 'w') as fp:
+            fp.write('###############################################\n')
+            fp.write('###############################################\n')
+            fp.write('Following files are stored to your local discs: \n')
+            fp.write('\n')
+            for resource in resources: 
+                fp.write('%s \n' % os.path.realpath(resource))
+        self.output.setValue( filename )
