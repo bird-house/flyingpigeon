8c08945f3c558a12e54b5e9d3bd570117b1fe6a8
Nils Hempelmann <info@nilshempelmann.de>
2016-02-18 14:38:11 +0100

    include mkstemp for txt file

--- flyingpigeon/processes/wps_get_resources.py
+++ flyingpigeon/processes/wps_get_resources.py
@@ -3,10 +3,11 @@ from pywps.Process import WPSProcess
 import logging
 
 class GetResourceProcess(WPSProcess):
+
     def __init__(self):
         WPSProcess.__init__(
             self, 
-            identifier = "wps_get_resources",
+            identifier = "get_resources",
             title="Download Resources",
             version = "0.1",
             abstract="This process downloads resources (limited to 50GB) to the local file system and returns a textfile with appropriate pathes",
@@ -18,41 +19,52 @@ class GetResourceProcess(WPSProcess):
             identifier="resource",
             title="Resource",
             abstract="NetCDF File",
-            minOccurs=1,
+            minOccurs=0,
             maxOccurs=1000,
             maxmegabites=50000,
             formats=[{"mimeType":"application/x-netcdf"}],
             )
 
-        self.output_text = self.addComplexOutput(
-            identifier="output_text",
-            title="Sourcefiles",
-            abstract="text file with a list of the used input data sets",
-            formats=[{"mimeType":"text/plain"}],
-            asReference=True,
-            )
-
+        self.pathfile = self.addComplexOutput(
+          identifier="pathfile",
+          title="Pathes File",
+          abstract="File containing the local pathes to downloades files",
+          formats=[{"mimeType":"text/plain"}],
+          asReference=True,
+          )
 
         def execute(self):
 
             from flyingpigeon import get_resources
+            from tempfile import mkstemp 
 
             self.status.set('starting write path files process', 0)
-
             urls = self.getInputValues(identifier='resource')
-
-            logger.info('urls: %s ' % urls)
-
-            self.status.set('Path for %s fiels ' % len(urls), 20)
+            
+            # logger.info('urls: %s ' % urls)
 
             try: 
-               resource_txt = get_resources.write_file(urls)
+                pid , pathfile = mkstemp(suffix='.txt', prefix='pathes_', dir='.', text=True)
+                pf = open(pathfile, mode='w')
+
+                logger.info('created the filepathes.txt file ')
+                pf.write('###############################################\n')
+                pf.write('###############################################\n')
+                pf.write('Following files are stored to your local discs: \n')
+                pf.write('\n')
+
+                # try: 
+                #   for url in urls: 
+                #     pathes.write('%s \n' % url)
+                # except Exception as e: 
+                #   logger.error('failed to write path to file')
+
+                textfile = pathes.name
+                pathes.close()
+
+                self.status.set('text file written', 90)
             except Exception as e: 
                 logger.error('failed to write file pathes ')
-                
 
-            self.status.set('text file written', 90)
-
-            self.output_text.setValue( resource_txt )
-
+            self.pathfile.setValue( pathfile )
             self.status.set('End of wps_get_resources process', 100)
