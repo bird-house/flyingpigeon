5fd41b7749c5c082a39880e2d89289fad538e34c
nils <info@nilshempelmann.de>
2016-03-10 18:28:10 +0100

    GAM prediction

--- scripts/gamR.py
+++ scripts/gamR.py
@@ -12,7 +12,7 @@ x2 = x1 / 2 + np.random.normal(2, 1, 1000)
 PA = x1
 PA[x1<15] = 0
 PA[x1>25] = 0
-PA[PA<0] = 1
+PA[PA>0] = 1
 
 x1 = x1 + np.random.normal(3, 1, 1000)
 
@@ -37,17 +37,17 @@ utils = importr('utils')
 d = {'PA': ro.IntVector(PA), 'x1': ro.FloatVector(x1),'x2': ro.FloatVector(x2) }
 dataf = ro.DataFrame(d)
 
-eq = ro.Formula("PA ~ s(x1, k = 3) + s(x2, k = 3)")
+eq = ro.Formula("PA ~ s(x1, k=3) + s(x2, k=3)")
 
 # from rpy2.rinterface import ListSexpVector
 #
 trans = ro.Formula('x ~ exp(x)/(1+exp(x))')
 
-trained_model = mgcv.gam(base.eval(eq),data=dataf, family=stats.binomial(), scale=-1)
+trained_model = mgcv.gam(base.eval(eq), data=dataf, family=stats.binomial(), scale=-1)
 
 for i in [1,2]:    
 #ylim = ro.IntVector([-6,6])
-  mgcv.plot_gam(trained_model, shade='T', trans=trans, col='black',select=i,ylab='Predicted Probability',rug=False , cex_lab = 1.4, cex_axis = 1.4, ) #ylim=ylim
+  mgcv.plot_gam(trained_model, shade='T', col='black',select=i,ylab='Predicted Probability',rug=False , cex_lab = 1.4, cex_axis = 1.4, ) #ylim=ylim,  trans=base.eval(trans),
   raw_input("Press Enter to continue...")
   grdevices.dev_off()
   
@@ -66,7 +66,7 @@ d2 = { 'x1': x1,'x2': x2}
 df = ro.DataFrame(d2)
 rstack = utils.stack(df)
 
-p = predict(gam_model, newdata=data.frame(x1=x1, x2=x2), type="response", progress="text", na_rm=True)
+p = mgcv.predict_gam(trained_model, newdata=df, type="response", progress="text", na_rm=True)
 
 #p <- predict(gam_model, newdata=df, type="response", progress="text", na.rm=TRUE)
 #species = stats.predict(object=rstack, model=trained_model, progress="text", na_rm=True, type="response")
