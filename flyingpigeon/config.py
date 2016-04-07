3b018ffc7f9b73693a935344fe42d56477ca621e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-04-07 15:08:37 +0200

    added cache_path()

--- flyingpigeon/config.py
+++ flyingpigeon/config.py
@@ -1,8 +1,21 @@
 import os
+from pywps import config as wpsconfig
+
+import logging
+logger = logging.getLogger(__name__)
 
 def shapefiles_dir():
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'shapefiles')
 
 def masks_dir():
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'masks')
+
+def cache_path():
+    cache_path = None
+    try:
+        cache_path = wpsconfig.getConfigValue("cache", "cache_path")
+    except:
+        logger.warn("No cache path configured. Using default value.")
+        cache_path = os.path.join(os.sep, "tmp", "cache")
+    return cache_path
 
