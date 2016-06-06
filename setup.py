333941a3d55b6f3e5530576d32dde8b59ab67644
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-06 11:36:08 +0200

    updated python requirements in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-requires = [
-    'cdo',
-    'ocgis',
-    'pandas',
-    'nose',
-    ]
+reqs = [line.strip() for line in open('requirements/deploy.txt')]
 
 classifiers=[
         'Development Status :: 3 - Alpha',
@@ -30,14 +25,14 @@ setup(name='flyingpigeon',
       classifiers=classifiers,
       author='Nils Hempelmann',
       author_email='nils.hempelmann@ipsl.jussieu.fr',
-      url='http://www.lsce.ipsl.fr/',
+      url='http://flyingpigeon.readthedocs.io/en/latest/',
       license = "http://www.apache.org/licenses/LICENSE-2.0",
-      keywords='wps flyingpigeon pywps malleefowl ipsl birdhouse conda anaconda',
+      keywords='wps flyingpigeon pywps ipsl birdhouse conda climate indices species',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       test_suite='nose.collector',
-      install_requires=requires,
+      install_requires=reqs,
       entry_points = {
           'console_scripts': [
               ]}     
