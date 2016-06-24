0d341e2fef2aff279e63493b4e541687f0783ee1
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-24 14:48:24 +0200

    add rsync to requirements

--- requirements.sh
+++ requirements.sh
@@ -2,10 +2,12 @@
 
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
+    sudo apt-get -y install rsync
     #sudo apt-get -y install gfortran
     #sudo apt-get -y install proj-bin proj-data
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
+    sduo yum -y install rsync
     #sudo yum -y install gcc-c++ gcc-gfortran
     #sudo yum -y install proj proj-epsg proj-nad
 elif [ `uname -s` = "Darwin" ] ; then
