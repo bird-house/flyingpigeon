c084ca3f3501700ef0b779a92be73558e711ad8c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-13 17:31:53 +0200

    fixed plot timeseries test

--- flyingpigeon/tests/test_wps_visualisation.py
+++ flyingpigeon/tests/test_wps_visualisation.py
@@ -1,13 +1,10 @@
-import nose.tools
-from nose.plugins.attrib import attr
+from .common import WpsTestClient, TESTDATA, assert_response_success
 
-from tests.common import WpsTestClient, TESTDATA, assert_response_success
-
-def test_wps_visualisation():
+def test_wps_plot_timeseries():
     wps = WpsTestClient()
     datainputs = "[resource={0};resource={1};variable=tasmax]".format(
         TESTDATA['cmip5_tasmax_r1_nc'], TESTDATA['cmip5_tasmax_r2_nc'])
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='visualisation',
+    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='plot_timeseries',
                    datainputs=datainputs)
     assert_response_success(resp)
 
