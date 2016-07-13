682bf5e25187c62f2ece6085de77227406a5d54b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-13 17:28:50 +0200

    fixed indices test

--- flyingpigeon/tests/test_indices.py
+++ flyingpigeon/tests/test_indices.py
@@ -1,10 +1,6 @@
-import nose.tools
-from unittest import TestCase
-from nose import SkipTest
-from nose.plugins.attrib import attr
+import pytest
 
-from tests.common import prepare_env, TESTDATA
-prepare_env()
+from .common import TESTDATA
 
 import os
 import tempfile
@@ -12,36 +8,28 @@ from netCDF4 import Dataset
 
 from flyingpigeon import indices
 from flyingpigeon.utils import local_path
+  
+def test_indices():
+    assert 'TG' in indices.indices()
 
-class IndicesCalculatorTestCase(TestCase):
+def test_indices_description():
+    assert 'TG: ' in indices.indices_description()
 
-    @classmethod
-    def setUpClass(cls):
-        pass
-        
-    def test_indices(self):
-        nose.tools.ok_( 'TG' in indices.indices(), indices.indices() )
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_indice_su_tasmax():
+    out_dir = tempfile.mkdtemp()
+    # SU expects tasmax
+    output = indices.calc_indice_single(
+        [local_path(TESTDATA['cordex_tasmax_nc'])],
+        indices=['SU'], groupings='year', dir_output=out_dir)
 
-    def test_indices_description(self):
-        nose.tools.ok_( 'TG: ' in indices.indices_description(), indices.indices_description() )
+    assert os.path.basename(output) == 'TG_EUR-44_MPI-M-MPI-ESM-LR_historical_r1i1p1_CLMcom-CCLM4-8-17_v1_day.nc'
 
-    def test_indice_su_tasmax(self):
-        raise SkipTest
-        out_dir = tempfile.mkdtemp()
-        # SU expects tasmax
-        output = indices.calc_indice_single(
-            [local_path(TESTDATA['cordex_tasmax_nc'])],
-            indices=['SU'], groupings='year', dir_output=out_dir)
-
-        nose.tools.ok_(
-            os.path.basename(output) == 'TG_EUR-44_MPI-M-MPI-ESM-LR_historical_r1i1p1_CLMcom-CCLM4-8-17_v1_day.nc',
-            output)
-        
-        ds = Dataset(output)
-        # SU variable must be in result
-        nose.tools.ok_('SU' in ds.variables, ds.variables.keys())
-        # 5 years
-        nose.tools.ok_(len(ds.variables['time']) == 5, len(ds.variables['time']))
+    ds = Dataset(output)
+    # SU variable must be in result
+    assert 'SU' in ds.variables
+    # 5 years
+    assert len(ds.variables['time']) == 5
 
 
 
