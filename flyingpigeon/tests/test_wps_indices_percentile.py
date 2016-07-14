a9e3002ac014b7a365e40e0682d27ec240fe0d1d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-14 15:51:01 +0200

    test for wps indices percentile added

---
+++ flyingpigeon/tests/test_wps_indices_percentile.py
@@ -0,0 +1,10 @@
+from .common import WpsTestClient, TESTDATA, assert_response_success
+
+def test_wps_indices_percentile():
+    wps = WpsTestClient()
+    datainputs = "[resource={0};resource={1};indices=TX_p]".format(
+        TESTDATA['cmip5_tasmax_2006_nc'], TESTDATA['cmip5_tasmax_2007_nc'])
+    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='indices_percentile',
+                   datainputs=datainputs)
+    assert_response_success(resp)
+
