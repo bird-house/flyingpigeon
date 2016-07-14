af26a8cd4ca8935611eacc542219b7237d181bc5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-14 14:38:49 +0200

    removed MFDataset from ocgis has_lambert

--- flyingpigeon/tests/test_ocgis.py
+++ flyingpigeon/tests/test_ocgis.py
@@ -1,3 +1,10 @@
+import pytest
+
+from .common import TESTDATA
+
+from flyingpigeon import ocgis_module
+from flyingpigeon.utils import local_path
+
 def test_ocgis_import():
     from ocgis import constants
 
@@ -5,5 +12,9 @@ def test_cdo_import():
     from cdo import Cdo
     cdo = Cdo()
 
-        
+def test_has_Lambert_Conformal():
+    has_lambert = ocgis_module.has_Lambert_Conformal(
+        [local_path(TESTDATA['cordex_tasmax_2006_nc']),
+        local_path(TESTDATA['cordex_tasmax_2007_nc'])])
+    assert False == has_lambert
   
