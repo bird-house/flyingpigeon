ed07553177e9ef2110fa3c48fab85e9b06b4a836
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-15 13:51:45 +0200

    removed links to unused testdata

--- flyingpigeon/tests/common.py
+++ flyingpigeon/tests/common.py
@@ -15,12 +15,6 @@ SERVICE = "http://localhost:8093/wps"
 TESTS_HOME = os.path.abspath(os.path.dirname(__file__))
 
 TESTDATA = { 
-    'noaa_nc_1': "http://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/surface/slp.1955.nc",
-    'noaa_catalog_1': "http://www.esrl.noaa.gov/psd/thredds/catalog/Datasets/ncep.reanalysis.dailyavgs/surface/catalog.xml?dataset=Datasets/ncep.reanalysis.dailyavgs/surface/air.sig995.1948.nc",
-    'noaa_slp_1955_nc': "http://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/surface/slp.1955.nc",
-    'noaa_slp_1956_nc': "http://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/surface/slp.1956.nc",
-    'eurocordex_nc_1': "http://webportals.ipsl.jussieu.fr/thredds/fileServer/ATLAS/Flux/LandModels/yearlymean/fco2_CLM4CN_Sep2013-ext3_1980-2010_yearlymean_XYT.nc",
-    'eurocordex_nc_2': "http://webportals.ipsl.jussieu.fr/thredds/fileServer/EUROCORDEX/extremoscope_FRA/polygons/TG/yr/rcp45/1/TG_rcp45_CNRM-CM5_CNRM-ALADIN53_1971-2100.nc",
     'cmip5_tasmax_2006_nc': "file://{0}".format(os.path.join(TESTS_HOME, 'testdata', 'cmip5', 'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200601-200612.nc')),
     'cmip5_tasmax_2007_nc': "file://{0}".format(os.path.join(TESTS_HOME, 'testdata', 'cmip5', 'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200701-200712.nc')),
     'cordex_tasmax_2006_nc': "file://{0}".format(os.path.join(TESTS_HOME, 'testdata', 'cordex', 'tasmax_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon_200602-200612.nc')),
