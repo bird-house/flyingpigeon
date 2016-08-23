f56b8528378500f584adb7d4390835e596be1e8a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-23 16:22:15 +0200

    reduced to a view countries

--- flyingpigeon/subset.py
+++ flyingpigeon/subset.py
@@ -15,7 +15,8 @@ def countries():
     """
     :return: a list of all countries codes.
     """
-    countries = _COUNTRIES_.keys()
+    #countries = _COUNTRIES_.keys()
+    countries = ['DEU', 'FRA', 'GBR', 'ESP', 'ITA']
     countries.sort()
     return countries  
 
