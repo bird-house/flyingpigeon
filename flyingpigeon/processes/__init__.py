20951ac61f12147f37f179542148f31b02f85873
nils <info@nilshempelmann.de>
2016-08-23 19:43:58 +0200

    add robustness to init

--- flyingpigeon/processes/__init__.py
+++ flyingpigeon/processes/__init__.py
@@ -13,13 +13,14 @@ __all__ = [
         "wps_segetalflora",
         "wps_sdm_gbif",
         "wps_sdm_csv",
+        "wps_robustness",
         "wps_plot_timeseries",
         "wps_fetch",
 # climate for impact processes
         "wps_c4i_simple_indice",
 # processes to be development     
 #        "wps_eobs2cordex",
-#        "wps_robustness",
+
 # TODO: c4i processes with multiple input sources
         #"wps_c4i_multivar_indice",
         #"wps_c4i_percentile_indice",
