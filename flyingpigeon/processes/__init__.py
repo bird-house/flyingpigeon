c4a40250f5b925cf2adb7f5966f44b14b9b096da
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-30 10:18:01 +0200

    activated subsetting of europe regions again

--- flyingpigeon/processes/__init__.py
+++ flyingpigeon/processes/__init__.py
@@ -1,7 +1,7 @@
 __all__ = [
         "wps_subset_continents",
         "wps_subset_countries",
-        #"wps_subset_regionseurope",
+        "wps_subset_regionseurope",
         "wps_subset_points",
         "wps_indices_simple",
         "wps_indices_percentile",
