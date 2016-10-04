82134414c49607baf246a489823134a6517d09e8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-04 13:34:13 +0200

    skip analogues detection test

--- flyingpigeon/tests/test_wps_analogs_detection.py
+++ flyingpigeon/tests/test_wps_analogs_detection.py
@@ -2,11 +2,13 @@ import pytest
 
 from .common import WpsTestClient, TESTDATA, assert_response_success
 
+
 @pytest.mark.online
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_wps_analogs_detection():
     wps = WpsTestClient()
     datainputs = "[dateSt=2013-07-15;dateEn=2013-12-31;refSt=2013-01-01;refEn=2013-12-31]"
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='analogs_detection',
+    resp = wps.get(service='wps', request='execute', version='1.0.0',
+                   identifier='analogs_detection',
                    datainputs=datainputs)
     assert_response_success(resp)
-
