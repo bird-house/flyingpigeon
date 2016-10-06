1af990b7119c10015833a2f206151c6d02bd1a3e
Nils Hempelmann <info@nilshempelmann.de>
2016-10-06 20:11:44 +0200

    example extraction

--- flyingpigeon/processes/wps_indices_simple.py
+++ flyingpigeon/processes/wps_indices_simple.py
@@ -131,13 +131,15 @@ class SingleIndicesProcess(WPSProcess):
             groupings=groupings,
             dir_output=path.curdir,
             ))
+         
+        results_list =  results.tolist()
 
         self.status.set('indices calculated', 90)
-        logger.debug('results type: %s', type(results))
-        logger.debug('indices files: %s ' % results.tolist())
+        logger.debug('results type: %s', type(results_list))
+        logger.debug('indices files: %s ' % results_list )
 
         try:
-            archive_indices = archive(results.tolist())
+            archive_indices = archive(results_list)
             logger.info('archive prepared')
         except Exception as e:
             msg = "archive preparation failed"
@@ -145,8 +147,14 @@ class SingleIndicesProcess(WPSProcess):
             raise Exception(msg)
         try: 
             self.output.setValue(archive_indices)
-            i = next((i for i, x in enumerate(results.tolist()) if x), None)
-            self.output_netcdf.setValue(str(results.tolist()[i]))
+            if type(results_list) == list:
+                i = next((i for i, x in enumerate(results.tolist()) if x), None)
+                self.output_netcdf.setValue(str(results[i]))
+            elif type(results_list) == str:
+                self.output_netcdf.setValue(results_list)
+            else:
+                logger.debug('results_list type: %s  not extractable ' % type(results_list) )
+                self.output_netcdf.setValue(None)
         except Exception as e:
             msg = "extraction of example file failed"
             logger.exception(msg)
--- flyingpigeon/utils.py
+++ flyingpigeon/utils.py
@@ -109,19 +109,18 @@ def archive(resources, format='tar', dir_output='.', mode='w'):
   """
   from tempfile import mkstemp
   from os.path import basename
-  from numpy import squeeze
 
   logger.info('compressing files to archive')
   try:
     if isinstance(resources, str):
       resources = list([resources])
-    resources = squeeze(resources)
-    resources_filter = [x for x in resources.tolist() if x is not None]
-    resources = resources_filter
 
+    resources_filter = [x for x in resources if x is not None]
+    resources = resources_filter
   except Exception as e:
     msg = 'failed to prepare file list: %s' % e
     logger.debug(msg)
+
 
   if format == 'tar':
     import tarfile
