9469cd2b3a1bf99f21f1012b42df4b9728d8c8a7
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-09 15:43:52 +0100

    added test_visualisation

---
+++ flyingpigeon/tests/test_visualisation.py
@@ -0,0 +1,10 @@
+from flyingpigeon import visualisation
+
+
+def test_map_gbifoccurrences():
+    from numpy import zeros
+    latlon = zeros((3, 2))
+    latlon[0] = [10, 20]
+    latlon[1] = [11, 21]
+    latlon[2] = [12, 22]
+    visualisation.map_gbifoccurrences(latlon=latlon)
