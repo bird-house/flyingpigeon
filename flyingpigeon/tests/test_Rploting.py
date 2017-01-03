745a246fdf16cfaf7c09d0cc76f990976f549b26
nilshempelmann <info@nilshempelmann.de>
2017-01-03 17:36:11 +0100

    R plot tests

---
+++ flyingpigeon/tests/test_Rploting.py
@@ -0,0 +1,44 @@
+import pytest
+from rpy2.robjects.packages import importr
+
+from os import remove
+grDevices = importr("grDevices")
+base = importr("base")
+
+
+def test_plain():
+    grDevices.pdf(file='Rplot.pdf')
+    grDevices.dev_off()
+    remove('Rplot.pdf')
+    png = grDevices.png(filename='Rplot.png', type='cairo')
+    grDevices.dev_off()
+    grDevices.postscript('Rplot.eps')
+    grDevices.dev_off()
+    remove('Rplot.eps')
+
+
+def test_pdf_garbage():
+    grDevices.pdf(file='Rplot.pdf')
+    garbage = grDevices.dev_off()
+    remove('Rplot.pdf')
+
+
+def test_png_garbage():
+    png = grDevices.png(filename='Rplot.png', type='cairo')
+    garbage = grDevices.dev_off()
+
+
+def test_png_sink():
+    logfile = "test_logfile.txt"
+    base.sink(logfile)
+    png = grDevices.png(filename='Rplot.png', type='cairo')
+    grDevices.dev_off()
+    unlink(logfile)
+
+
+def test_png_invisible():
+    logfile = "test_logfile.txt"
+    base.sink(logfile)
+    png = grDevices.png(filename='Rplot.png', type='cairo')
+    grDevices.dev_off()
+    unlink(logfile)
