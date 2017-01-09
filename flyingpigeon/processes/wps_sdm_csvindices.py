d4ba8f2f49c67d0b8735c38b3fdf29d92e06a9d9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-09 14:03:49 +0100

    using init_process_logger() to collect log messages and return it as output_log

--- flyingpigeon/processes/wps_sdm_csvindices.py
+++ flyingpigeon/processes/wps_sdm_csvindices.py
@@ -8,6 +8,7 @@ import tempfile
 from pywps.Process import WPSProcess
 
 from flyingpigeon.sdm import _SDMINDICES_
+from flyingpigeon.log import init_process_logger
 
 import logging
 logger = logging.getLogger(__name__)
@@ -137,10 +138,21 @@ class SDMcsvindicesProcess(WPSProcess):
             asReference=True,
         )
 
+        self.output_log = self.addComplexOutput(
+            identifier="output_log",
+            title="Logging information",
+            abstract="Collected logs during process run.",
+            formats=[{"mimeType": "text/plain"}],
+            asReference=True,
+        )
+
     def execute(self):
         from os.path import basename
         from flyingpigeon import sdm
         from flyingpigeon.utils import archive, archiveextract  # , get_domain
+
+        init_process_logger('log.txt')
+        self.output_log.setValue('log.txt')
 
         self.status.set('Start process', 0)
 
