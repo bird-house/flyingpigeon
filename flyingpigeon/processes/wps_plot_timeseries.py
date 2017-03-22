8bed6c6e2f72ec0eb566c7695b64d049f3626be2
nilshempelmann <info@nilshempelmann.de>
2017-03-22 10:44:54 +0100

    pywps4 plot timeseries

--- flyingpigeon/processes/wps_plot_timeseries.py
+++ flyingpigeon/processes/wps_plot_timeseries.py
@@ -1,86 +1,91 @@
 from flyingpigeon import visualisation as vs
-from pywps.Process import WPSProcess
+from flyingpigeon.utils import archiveextract
+
+
+from pywps import Process
+from pywps import LiteralInput
+from pywps import ComplexInput, ComplexOutput
+from pywps import Format
+from pywps.inout.literaltypes import AllowedValue
+from pywps.app.Common import Metadata
 
 from flyingpigeon.log import init_process_logger
 
 import logging
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger("PYWPS")
 
 
-class plottimeseriesProcess(WPSProcess):
+class PlottimeseriesProcess(Process):
     def __init__(self):
-        # definition of this process
-        WPSProcess.__init__(
-            self,
+        inputs = [
+            ComplexInput('resource', 'Resource',
+                         abstract='NetCDF Files or archive (tar/zip) containing NetCDF files.',
+                         metadata=[Metadata('Info')],
+                         min_occurs=1,
+                         max_occurs=1000,
+                         supported_formats=[
+                             Format('application/x-netcdf'),
+                             Format('application/x-tar'),
+                             Format('application/zip'),
+                         ]),
+
+
+            LiteralInput("variable", "Variable",
+                         abstract="Variable to be expected in the input files (variable will be detected if not set)",
+                         default=None,
+                         data_type='string',
+                         min_occurs=0,
+                         max_occurs=1,
+                         ),
+        ]
+        outputs = [
+            ComplexOutput('output_log', 'Logging information',
+                          abstract="Collected logs during process run.",
+                          as_reference=True,
+                          supported_formats=[Format('text/plain')]
+                          ),
+
+            ComplexOutput("plotout_spagetti", "Visualisation, Spaghetti plot",
+                          abstract="Visualisation of single variables as a spaghetti plot",
+                          supported_formats=[Format("image/png")],
+                          as_reference=True,
+                          )
+
+            ComplexOutput("plotout_uncertainty", "Visualisation, Uncertainty plot",
+                          abstract="Visualisation of single variables ensemble mean with uncertainty",
+                          supported_formats=[Format("image/png")],
+                          as_reference=True,
+                          )
+        ]
+
+        super(PlottimeseriesProcess, self).__init__(
+            self._handler,
             identifier="plot_timeseries",
-            title="Plots -- timeseries",
-            version="0.9",
+            title="Graphics (timeseries)",
+            version="0.10",
             metadata=[
-                    {"title": 'Plots timeseries'}
+                Metadata('Doc', 'http://flyingpigeon.readthedocs.io/en/latest/'),
                     ],
             abstract="Outputs some timeseries of the file field means. Spaghetti and uncertainty plot",
-            statusSupported=True,
-            storeSupported=True
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
             )
 
-        self.resource = self.addComplexInput(
-            identifier="resource",
-            title="NetCDF Files",
-            abstract="NetCDF Files",
-            minOccurs=1,
-            maxOccurs=100,
-            maxmegabites=5000,
-            formats=[{"mimeType": "application/x-netcdf"}],
-            )
-
-        self.variableIn = self.addLiteralInput(
-            identifier="variable",
-            title="Variable",
-            abstract="Variable to be expected in the input files (variable will be detected if not set)",
-            default=None,
-            type=type(''),
-            minOccurs=0,
-            maxOccurs=1,
-            )
-
-        self.plotout_spagetti = self.addComplexOutput(
-            identifier="plotout_spagetti",
-            title="Visualisation, Spaghetti plot",
-            abstract="Visualisation of single variables as a spaghetti plot",
-            formats=[{"mimeType": "image/png"}],
-            asReference=True,
-            )
-
-        self.plotout_uncertainty = self.addComplexOutput(
-            identifier="plotout_uncertainty",
-            title="Visualisation, Uncertainty plot",
-            abstract="Visualisation of single variables ensemble mean with uncertainty",
-            formats=[{"mimeType": "image/png"}],
-            asReference=True,
-            )
-
-        self.output_log = self.addComplexOutput(
-            identifier="output_log",
-            title="Logging information",
-            abstract="Collected logs during process run.",
-            formats=[{"mimeType": "text/plain"}],
-            asReference=True,
-        )
-
-    def execute(self):
-
+    def _handler(self, request, response):
         init_process_logger('log.txt')
-        self.output_log.setValue('log.txt')
-        from flyingpigeon.utils import archiveextract
+        response.outputs['output_log'].file = 'log.txt'
 
-        ncfiles = archiveextract(self.getInputValues(identifier='resource'))
-        var = self.variableIn.getValue()
+        ncfiles = archiveextract(
+            resource=rename_complexinputs(request.inputs['resource']))
+        var = request.inputs['variableIn']
 
         if var is None:
             from flyingpigeon.utils import get_variable
             var = get_variable(ncfiles[0])
 
-        self.status.set('plotting variable %s' % var, 10)
+        response.update_status('plotting variable %s' % var, 10)
 
         try:
             plotout_spagetti_file = vs.spaghetti(
@@ -89,10 +94,10 @@ class plottimeseriesProcess(WPSProcess):
                                              title='Fieldmean of %s ' % (var),
                                              dir_out=None
                                              )
-            logger.info("spagetti plot done")
-            self.status.set('Spagetti plot for %s %s files done' % (len(ncfiles), var), 50)
+            LOGGER.info("spagetti plot done")
+            response.update_status('Spagetti plot for %s %s files done' % (len(ncfiles), var), 50)
         except:
-            logger.exception("spagetti plot failed")
+            LOGGER.exception("spagetti plot failed")
 
         try:
             plotout_uncertainty_file = vs.uncertainty(
@@ -102,11 +107,11 @@ class plottimeseriesProcess(WPSProcess):
                                                   dir_out=None
                                                   )
 
-            self.status.set('Uncertainty plot for %s %s files done' % (len(ncfiles), var), 90)
-            logger.info("uncertainty plot done")
+            response.update_status('Uncertainty plot for %s %s files done' % (len(ncfiles), var), 90)
+            LOGGER.info("uncertainty plot done")
         except:
-            logger.exception("uncertainty plot failed")
+            LOGGER.exception("uncertainty plot failed")
 
-        self.plotout_spagetti.setValue(plotout_spagetti_file)
-        self.plotout_uncertainty.setValue(plotout_uncertainty_file)
-        self.status.set('visualisation done', 100)
+        response.outputs['plotout_spagetti'].file = plotout_spagetti_file
+        response.outputs['plotout_uncertainty'].file = plotout_uncertainty_file
+        response.update_status('visualisation done', 100)
