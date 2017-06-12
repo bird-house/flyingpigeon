42313283fc85f864a91e08f29401521d061e2720
nilshempelmann <info@nilshempelmann.de>
2017-06-12 20:52:19 +0100

    percentile based indices update

--- scripts/ocgis_indices_percentile.py
+++ scripts/ocgis_indices_percentile.py
@@ -12,8 +12,7 @@ from flyingpigeon.utils import get_values, get_time
 from numpy import ma
 import uuid
 
-p = '/home/nils/birdhouse/var/lib/pywps/cache/malleefowl/esgf1.dkrz.de/thredds/fileServer/cordex/cordex/output/AFR-44/\
-     MPI-CSC/MPI-M-MPI-ESM-LR/historical/r1i1p1/MPI-CSC-REMO2009/v1/day/tas/v20160412/'
+p = '/home/nils/birdhouse/var/lib/pywps/cache/malleefowl/esgf1.dkrz.de/thredds/fileServer/cordex/cordex/output/AFR-44/MPI-CSC/MPI-M-MPI-ESM-LR/historical/r1i1p1/MPI-CSC-REMO2009/v1/day/tas/v20160412/'
 
 resource = [path.join(p, nc) for nc in listdir(p)]
 resource.sort()
@@ -28,7 +27,7 @@ dt2 = dt(2000, 12, 31)
 period = [dt1, dt2]  # we will calculate the indice for 10 years
 #
 
-rd = RequestDataset(recource, 'tas', time_range=period)
+rd = RequestDataset(resource, 'tas', time_range=period)
 
 from ocgis.constants import DimensionMapKey
 rd.dimension_map.set_bounds(DimensionMapKey.TIME, None)
@@ -36,6 +35,8 @@ rd.dimension_map.set_bounds(DimensionMapKey.TIME, None)
 
 kwds = {'percentile': 90, 'window_width': 5}
 calc = [{'func': 'daily_perc', 'name': 'dp', 'kwds': kwds}]
-ops = ocgis.OcgOperations(dataset=rd, geom='state_boundaries', select_ugid=[23], calc=calc,
-                          output_format='nc', time_region={'year': [1980, 1990]}).execute()
+ops = OcgOperations(dataset=rd, calc=calc,
+                    output_format='nc',
+                    time_region={'year': [1980, 1990]}
+                    ).execute()
 print ops
