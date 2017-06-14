16a00d09a9a2e2308f2198f0cdb8f3b1946a7f53
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-14 15:51:49 +0200

    skip R tests

--- flyingpigeon/tests/test_R.py
+++ flyingpigeon/tests/test_R.py
@@ -1,34 +1,19 @@
 import pytest
 
-from rpy2.robjects.packages import importr
 
-
+@pytest.mark.skip(reason="R not available")
 def test_R_rpy2():
     import rpy2.robjects
     import rpy2.robjects.numpy2ri
     rpy2.robjects.numpy2ri.activate()
 
 
-def test_R_fields():
+@pytest.mark.skip(reason="R not available")
+def test_R_imports():
+    from rpy2.robjects.packages import importr
     fields = importr("fields")
-
-
-def test_R_stats():
     stats = importr("stats")
-
-
-def test_R_ncdf4():
     ncdf4 = importr("ncdf4")
-
-
-def test_R_mclust():
     mclust = importr("mclust")
-
-
-def test_R_maps():
     maps = importr("maps")
-
-
-def test_grDevices():
-    from os import remove
     grDevices = importr("grDevices")
