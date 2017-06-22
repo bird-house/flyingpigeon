053c487929371aa9286ace328f95ff83cf9455b2
nilshempelmann <info@nilshempelmann.de>
2017-06-22 08:25:15 +0100

    netCDF4 test

--- scripts/ocgis_indices_percentile.py
+++ scripts/ocgis_indices_percentile.py
@@ -12,8 +12,8 @@ from flyingpigeon.utils import get_values, get_time
 from numpy import ma
 import uuid
 
-p = '/home/nils/birdhouse/var/lib/pywps/cache/malleefowl/esgf1.dkrz.de/thredds/fileServer/cordex/cordex/output/AFR-44/MPI-CSC/MPI-M-MPI-ESM-LR/historical/r1i1p1/MPI-CSC-REMO2009/v1/day/tas/v20160412/'
-
+# p = '/home/nils/birdhouse/var/lib/pywps/cache/malleefowl/esgf1.dkrz.de/thredds/fileServer/cordex/cordex/output/AFR-44/MPI-CSC/MPI-M-MPI-ESM-LR/historical/r1i1p1/MPI-CSC-REMO2009/v1/day/tas/v20160412/'
+p = '/home/nils/birdhouse/var/lib/pywps/cache/malleefowl/esgf1.dkrz.de/thredds/fileServer/cordex/cordex/output/AFR-44/CLMcom/CNRM-CERFACS-CNRM-CM5/historical/r1i1p1/CLMcom-CCLM4-8-17/v1/day/tas/v20140401/'
 resource = [path.join(p, nc) for nc in listdir(p)]
 resource.sort()
 # rd = RequestDataset(ncs[0])
