1dcee8205ca6a6eb6d261e7c2fe3af564cc3480b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-30 12:31:54 +0200

    pep8

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 reqs = [line.strip() for line in open('requirements/deploy.txt')]
 
-classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        ]
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+]
 
 setup(name='flyingpigeon',
       version='0.2.1',
@@ -26,15 +26,13 @@ setup(name='flyingpigeon',
       author='Nils Hempelmann',
       author_email='nils.hempelmann@ipsl.jussieu.fr',
       url='http://flyingpigeon.readthedocs.io/en/latest/',
-      license = "http://www.apache.org/licenses/LICENSE-2.0",
+      license="http://www.apache.org/licenses/LICENSE-2.0",
       keywords='wps flyingpigeon pywps ipsl birdhouse conda climate indices species',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       test_suite='flyingpigeon',
       install_requires=reqs,
-      entry_points = {
-          'console_scripts': [
-              ]}     
-      ,
+      entry_points={
+          'console_scripts': []},
       )
