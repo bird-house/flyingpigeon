a3148770c8c9b7ea66ee56640024dcd23958e275
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-08 11:49:13 +0200

    using static_url

--- flyingpigeon/templating.py
+++ flyingpigeon/templating.py
@@ -10,6 +10,23 @@ TEMPLATES = Environment(loader=FileSystemLoader(template_path),
                         autoescape=True)
 
 
+def update_template_context(context):
+    """Update the template context with some commonly used variables.
+    The original values in the context will not be overridden if a
+    context processor decides to return a value with the same key.
+
+    :param context: the context as a dictionary that is updated in place
+                    to add extra variables.
+    """
+    common_vars = dict(static_url='/static')
+    orig_ctx = context.copy()
+    context.update(common_vars)
+    # make sure the original values win.  This makes it possible to
+    # easier add new variables in context processors without breaking
+    # existing views.
+    context.update(orig_ctx)
+
+
 def render_template(template_name, **context):
     """Renders a template from the template folder with the given
     context.
@@ -19,4 +36,5 @@ def render_template(template_name, **context):
                     context of the template.
     """
     template = TEMPLATES.get_template('analogviewer.html')
+    update_template_context(context)
     return template.render(context)
