8da04e6ed36961fc9cb065f76058597fc89ab849
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-09 17:50:45 +0200

    added daemon mode

--- flyingpigeon/demo.py
+++ flyingpigeon/demo.py
@@ -1,6 +1,8 @@
 import os
 
 from pywps import configuration
+
+from flyingpigeon import wsgi
 from flyingpigeon._compat import urlparse
 
 import logging
@@ -23,10 +25,26 @@ def get_host():
     return bind_host, port
 
 
+def _run(application):
+    from werkzeug.serving import run_simple
+    # call this *after* app is initialized ... needs pywps config.
+    bind_host, port = get_host()
+    # need to serve the wps outputs
+    static_files = {
+        # '/static': ('flyingpigeon', 'static'),
+        '/outputs': configuration.get_config_value('server', 'outputpath')
+    }
+    run_simple(
+        hostname=bind_host,
+        port=port,
+        application=application,
+        use_debugger=True,
+        use_reloader=True,
+        static_files=static_files)
+
+
 def main():
     import argparse
-    from werkzeug.serving import run_simple
-    from flyingpigeon import wsgi
 
     parser = argparse.ArgumentParser(
         description="""Script for starting a demo Flyingpigeon WPS
@@ -51,21 +69,25 @@ def main():
     if args.debug:
         cfg_files.append(os.path.join(os.path.dirname(__file__), 'debug.cfg'))
     app = wsgi.create_app(cfg_files)
-    static_files = {
-        # '/static': ('flyingpigeon', 'static'),
-        '/outputs': configuration.get_config_value('server', 'outputpath')
-    }
+    # let's start the service ...
+    if args.daemon:
+        # daemon (fork) mode
+        pid = None
+        try:
+            pid = os.fork()
+            if pid:
+                LOGGER.warn('forked process id: %s', pid)
+        except OSError as e:
+            raise Exception("%s [%d]" % (e.strerror, e.errno))
 
-    # call this *after* app is initialized ... needs pywps config.
-    bind_host, port = get_host()
-
-    run_simple(
-        hostname=bind_host,
-        port=port,
-        application=app,
-        use_debugger=True,
-        use_reloader=True,
-        static_files=static_files)
+        if (pid == 0):
+            os.setsid()
+            _run(app)
+        else:
+            os._exit(0)
+    else:
+        # no daemon
+        _run(app)
 
 
 if __name__ == '__main__':
