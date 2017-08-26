296b6c7a5d5b350ef278f761a63c3e79a97c23c0
nilshempelmann <info@nilshempelmann.de>
2017-08-26 17:10:30 +0100

    sort config path alphabetical

--- flyingpigeon/config.py
+++ flyingpigeon/config.py
@@ -8,12 +8,16 @@ import logging
 LOGGER = logging.getLogger("PYWPS")
 
 
-def esgfsearch_url():
-    url = configuration.get_config_value("extra", "esgfsearch_url")
-    if not url:
-        LOGGER.warn("No ESGF Search URL configured. Using default value.")
-        url = 'https://esgf-data.dkrz.de/esg-search'
-    return url
+def cache_path():
+    cache_path = configuration.get_config_value("cache", "cache_path")
+    if not cache_path:
+        LOGGER.warn("No cache path configured. Using default value.")
+        cache_path = os.path.join(configuration.get_config_value("server", "outputpath"), "cache")
+    return cache_path
+
+
+def data_path():
+    return os.path.join(_PATH, 'data')
 
 
 def esgfsearch_distrib():
@@ -24,33 +28,17 @@ def esgfsearch_distrib():
     return distrib
 
 
-def static_path():
-    return os.path.join(_PATH, 'static')
-
-
-def data_path():
-    return os.path.join(_PATH, 'data')
-
-
-def shapefiles_path():
-    return os.path.join(data_path(), 'shapefiles')
+def esgfsearch_url():
+    url = configuration.get_config_value("extra", "esgfsearch_url")
+    if not url:
+        LOGGER.warn("No ESGF Search URL configured. Using default value.")
+        url = 'https://esgf-data.dkrz.de/esg-search'
+    return url
 
 
 def masks_path():
     # TODO: currently this folder is not used
     return os.path.join(data_path(), 'masks')
-
-
-def Rsrc_dir():
-    return os.path.join(_PATH, 'Rsrc')
-
-
-def cache_path():
-    cache_path = configuration.get_config_value("cache", "cache_path")
-    if not cache_path:
-        LOGGER.warn("No cache path configured. Using default value.")
-        cache_path = os.path.join(configuration.get_config_value("server", "outputpath"), "cache")
-    return cache_path
 
 
 def output_path():
@@ -62,3 +50,19 @@ def output_url():
     if url:
         url = url.rstrip('/')
     return url
+
+
+def Rsrc_dir():
+    return os.path.join(_PATH, 'Rsrc')
+
+
+def shapefiles_path():
+    return os.path.join(data_path(), 'shapefiles')
+
+
+def static_path():
+    return os.path.join(_PATH, 'static')
+
+
+def testdata_path():
+    return os.path.join(data_path(), 'tests/testdata')
