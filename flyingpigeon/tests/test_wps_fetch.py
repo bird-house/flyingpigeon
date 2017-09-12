98de82289f56ffd8bcb1057ef10f36e817c644f0
David Huard <david.huard@gmail.com>
2017-09-12 15:09:57 -0400

    fixed test_wps_fetch

--- flyingpigeon/tests/test_wps_fetch.py
+++ flyingpigeon/tests/test_wps_fetch.py
@@ -6,14 +6,11 @@ from pywps.tests import assert_response_success
 from .common import TESTDATA, client_for
 from flyingpigeon.processes import FetchProcess
 
-
-@pytest.mark.online
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_wps_fetch():
     client = client_for(Service(processes=[FetchProcess()]))
-    datainputs = "resource@xlink:href={0}".format(TESTDATA['cmip5_tasmax_2006_nc'])
+    datainputs = "resource=files@xlink:href={0}".format(TESTDATA['cmip5_tasmax_2006_nc'])
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0',
-        identifier='fetch',
+        identifier='fetch_resources',
         datainputs=datainputs)
     assert_response_success(resp)
