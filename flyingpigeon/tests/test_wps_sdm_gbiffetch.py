887bf91959d5bbeef9d7d70ac6ff5b853bb15b75
David Huard <david.huard@gmail.com>
2017-09-12 15:43:15 -0400

    skip test for sdm_gbiffetch until BBox support is added so download time is reduced.

--- flyingpigeon/tests/test_wps_sdm_gbiffetch.py
+++ flyingpigeon/tests/test_wps_sdm_gbiffetch.py
@@ -3,15 +3,15 @@ import pytest
 from pywps import Service
 from pywps.tests import assert_response_success
 
-from .common import TESTDATA, client_for
+from .common import client_for
 from flyingpigeon.processes import GBIFfetchProcess
 
-
+@pytest.mark.skip # This is much too long. I suggest we create a test that requires less data to be downloaded.
+@pytest.mark.slow
 @pytest.mark.online
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_wps_sdm_gbiffetch():
     client = client_for(Service(processes=[GBIFfetchProcess()]))
-    datainputs = "taxon_name=Fagus sylvatica;BBox=-10,20,10,40;"
+    datainputs = "taxon_name=Fagus sylvatica;BBox=-10,20,10,40;" # BBox is not supported in the process.
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0',
         identifier='sdm_gbiffetch',
