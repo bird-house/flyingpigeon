295bc547a63874e651056879dc91ae9ec010c738
David Huard <david.huard@gmail.com>
2017-09-12 15:49:06 -0400

    fixed test_indices

--- flyingpigeon/tests/test_indices.py
+++ flyingpigeon/tests/test_indices.py
@@ -18,7 +18,6 @@ def test_indices_description():
     assert 'TG: ' in indices.indices_description()
 
 
-# @pytest.mark.skip(reason="no way of currently testing this")
 def test_indice_simple():
     # SU expects tasmax
     resources = [local_path(TESTDATA['cordex_tasmax_2006_nc'])]
@@ -35,18 +34,15 @@ def test_indice_simple():
     assert len(ds.variables['time']) == 1
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_indice_percentile():
     # TX90p expects tasmax
     resources = [local_path(TESTDATA['cordex_tasmax_2006_nc'])]
     output = indices.calc_indice_percentile(
         resources, variable='tasmax',
-        indices=['TX'], percentile=90, groupings='year',
+        indices=['TX'], percentile=90, grouping='year',
         dir_output=tempfile.mkdtemp())
 
-    assert os.path.basename(output[0]) == 'TX_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon_200602-200612.nc'  # noqa
-
-    ds = Dataset(output[0])
+    ds = Dataset(output)
     # SU variable must be in result
     assert 'TX' in ds.variables
     # 1 year
