cbf2b0104320365b1756d167cbe915e09c52a73d
David Huard <david.huard@gmail.com>
2017-09-12 15:30:04 -0400

    fixed tests for indices_simple and indices_percentile.

--- flyingpigeon/tests/test_wps_indices_percentile.py
+++ flyingpigeon/tests/test_wps_indices_percentile.py
@@ -7,10 +7,10 @@ from .common import TESTDATA, client_for
 from flyingpigeon.processes import IndicespercentiledaysProcess
 
 
-@pytest.mark.skip(reason="ocgis fails in this test")
 def test_wps_indices_percentiledays():
     client = client_for(Service(processes=[IndicespercentiledaysProcess()]))
-    datainputs = "resource@xlink:href={0};percentile=90".format(TESTDATA['cordex_tasmax_2006_nc'])
+    datainputs = "resource=files@xlink:href={0};percentile=90"\
+        .format(TESTDATA['cordex_tasmax_2006_nc'])
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0',
         identifier='indices_percentiledays',
--- flyingpigeon/tests/test_wps_indices_simple.py
+++ flyingpigeon/tests/test_wps_indices_simple.py
@@ -7,12 +7,12 @@ from .common import TESTDATA, client_for
 from flyingpigeon.processes import IndicessingleProcess
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_wps_indices_simple():
     client = client_for(Service(processes=[IndicessingleProcess()]))
-    datainputs = "resource@xlink:href={0};indices=SU".format(TESTDATA['cordex_tasmax_2006_nc'])
+    datainputs = "resource=files@xlink:href={0};indices=SU;grouping=yr".\
+        format(TESTDATA['cordex_tasmax_2006_nc'])
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0',
-        identifier='indices_simple',
+        identifier='indices_single',
         datainputs=datainputs)
     assert_response_success(resp)
