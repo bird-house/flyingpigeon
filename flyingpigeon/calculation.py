930e48afa950e9ab8871228d234d76d8b95965ea
Nikolay Kadygrov <nkadyg@lsce3240.lsce.ipsl.fr>
2017-11-17 13:49:46 +0100

    add detrending for 3D field

--- flyingpigeon/calculation.py
+++ flyingpigeon/calculation.py
@@ -1,14 +1,12 @@
-
 import logging
 LOGGER = logging.getLogger("PYWPS")
 
-
 from flyingpigeon.config import testdata_path
 
-from flyingpigeon.config import testdata_path
 from os import path
 from os import listdir
 
+import numpy as np
 
 def fieldmean(resource):
     """
@@ -49,3 +47,71 @@ def fieldmean(resource):
     else:
         LOGGER.error('not 3D shaped data. Average can not be calculated')
     return meanTimeserie
+
+def remove_mean_trend(fana, varname):
+    """
+    Removing the smooth trend from 3D netcdf file
+    """
+    from cdo import Cdo
+    from netCDF4 import Dataset
+    import uuid
+    from scipy.interpolate import UnivariateSpline
+    from os import system
+
+    if type(fana) == list:
+        fana = fana[0]
+
+    backup_ana = 'orig_mod_'+path.basename(fana)
+
+    cdo = Cdo()
+
+    # create backup of input file
+    # Again, an issue with cdo versioning.
+    # TODO: Fix CDO versioning workaround...
+
+    try:
+        cdo_cp = getattr(cdo,'copy')
+        cdo_cp(input=fana, output=backup_ana)
+    except:
+        if(path.isfile(backup_ana)==False):
+            com='copy'
+            comcdo = 'cdo -O %s %s %s' % (com, fana, backup_ana)
+            system(comcdo)
+        else:
+            backup_ana = 'None'
+
+    # create fmana - mean field
+    fmana = '%s.nc' % uuid.uuid1()
+
+    cdo_op = getattr(cdo,'fldmean')
+    cdo_op(input=fana, output=fmana)
+
+    mean_arc_dataset = Dataset(fmana)
+    mean_arcvar = mean_arc_dataset.variables[varname][:]
+    data=mean_arcvar[:,0,0]
+    mean_arc_dataset.close()    
+    x = np.linspace(0,len(data)-1,len(data))
+    y = data
+
+    # Very slow method. 
+    # TODO: sub by fast one 
+    # (there is one in R, but doesn't want to add R to analogs...)
+    spl = UnivariateSpline(x,y)
+
+    smf = (len(y)) * np.var(y)
+    spl.set_smoothing_factor(smf)
+    trend = np.zeros(len(y),dtype=np.float)
+    trend[:] = spl(x)
+
+    orig_arc_dataset = Dataset(fana,'r+')
+    orig_arcvar = orig_arc_dataset.variables.pop(varname)
+    orig_data = orig_arcvar[:]
+
+    det = np.zeros(np.shape(orig_data),dtype=np.float)
+    det = (orig_data.T - trend).T
+
+    orig_arcvar[:] = det
+    orig_arc_dataset.close()
+
+    return backup_ana
+
