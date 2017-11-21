0eec3665c9ea80e5a6e56c28b0e795a34fc4ac96
nilshempelmann <info@nilshempelmann.de>
2017-11-21 17:10:09 +0100

    adopt geojson

--- scripts/fetch_planet.py
+++ scripts/fetch_planet.py
@@ -1,29 +1,82 @@
-# Stockton, CA bounding box (created via geojson.io)
-geojson_geometry = {
-  "type": "Polygon",
-  "coordinates": [
-    [
-      [-121.59290313720705, 37.93444993515032],
-      [-121.27017974853516, 37.93444993515032],
-      [-121.27017974853516, 38.065932950547484],
-      [-121.59290313720705, 38.065932950547484],
-      [-121.59290313720705, 37.93444993515032]
-    ]
-  ]
-}
+import os
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from tempfile import mkstemp
+import shutil
 
+# Stockton, CA bounding box (created via geojson.io)
 # geojson_geometry = {
 #   "type": "Polygon",
 #   "coordinates": [
 #     [
-#         [9.00, 14.30],
-#         [8.30, 14.30],
-#         [8.30, 15.00],
-#         [9.00, 15.00],
-#         [9.00, 14.30],
-#      ]
+#       [-121.59290313720705, 37.93444993515032],
+#       [-121.27017974853516, 37.93444993515032],
+#       [-121.27017974853516, 38.065932950547484],
+#       [-121.59290313720705, 38.065932950547484],
+#       [-121.59290313720705, 37.93444993515032]
+#     ]
 #   ]
 # }
+#
+
+geojson_geometry = {
+  "type": "Polygon",
+  "coordinates": [
+            [
+              [
+                14.38385009765625,
+                8.339235543309647
+              ],
+              [
+                14.940032958984375,
+                8.339235543309647
+              ],
+              [
+                14.940032958984375,
+                9.087180443666833
+              ],
+              [
+                14.38385009765625,
+                9.087180443666833
+              ],
+              [
+                14.38385009765625,
+                8.339235543309647
+              ]
+            ]]
+}
+#
+#
+# geojson_geometry = {
+#   "type": "Polygon",
+#   "coordinates": [
+#           [
+#             [
+#               14.600830078125,
+#               8.677421123289992
+#             ],
+#             [
+#               14.797210693359375,
+#               8.677421123289992
+#             ],
+#             [
+#               14.797210693359375,
+#               8.90678000752024
+#             ],
+#             [
+#               14.600830078125,
+#               8.90678000752024
+#             ],
+#             [
+#               14.600830078125,
+#               8.677421123289992
+#             ]
+#           ]
+#         ]
+# }
+
+
 # get images that overlap with our AOI
 geometry_filter = {
   "type": "GeometryFilter",
@@ -31,13 +84,21 @@ geometry_filter = {
   "config": geojson_geometry
 }
 
+from datetime import datetime as dt
+from datetime import datetime, timedelta
+
+t = dt.now()
+
+y = t - timedelta(days=1)
+yy = t - timedelta(days=11)
+
 # get images acquired within a date range
 date_range_filter = {
   "type": "DateRangeFilter",
   "field_name": "acquired",
   "config": {
-    "gte": "2017-08-31T00:00:00.000Z",
-    "lte": "2017-09-01T00:00:00.000Z"
+    "gte": "%sT00:00:00.000Z" % (yy.strftime('%Y-%m-%d')),
+    "lte": "%sT00:00:00.000Z" % (y.strftime('%Y-%m-%d'))
   }
 }
 
@@ -55,10 +116,7 @@ combined_filter = {
   "type": "AndFilter",
   "config": [geometry_filter, date_range_filter, cloud_cover_filter]
 }
-import os
-import json
-import requests
-from requests.auth import HTTPBasicAuth
+
 
 # API Key stored as an env variable
 PLANET_API_KEY = os.getenv('PL_API_KEY')
@@ -94,32 +152,33 @@ for image_id in image_ids:
     # List of asset types available for this particular satellite image
     print(result.json().keys())
     # This is "inactive" if the "visual" asset has not yet been activated; otherwise 'active'
-    # print(result.json()['visual']['status'])
-    # # Parse out useful links
-    # links = result.json()[u"visual"]["_links"]
-    # self_link = links["_self"]
-    # activation_link = links["activate"]
-    #
-    # # Request activation of the 'visual' asset:
-    # activate_result = requests.get(activation_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
-    # # Parse out useful links
-    # links = result.json()[u"visual"]["_links"]
-    # self_link = links["_self"]
-    # activation_link = links["activate"]
-    #
-    # # Request activation of the 'visual' asset:
-    # activate_result = requests.get(activation_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
-    # # Parse out useful links
-    # links = result.json()[u"visual"]["_links"]
-    # self_link = links["_self"]
-    # activation_link = links["activate"]
-    #
-    # # Request activation of the 'visual' asset:
-    # activate_result = requests.get(activation_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
-    # activation_status_result = requests.get(self_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
-    #
-    # print(activation_status_result.json()["status"])
-    #
-    # # Image can be downloaded by making a GET with your Planet API key, from here:
-    # download_link = activation_status_result.json()["location"]
-    # print(download_link)
+    if 'visual' in  result.json().keys():
+        print "****** down loading file ********"
+        print(result.json()['visual']['status'])
+        # Parse out useful links
+        links = result.json()[u"visual"]["_links"]
+        self_link = links["_self"]
+        activation_link = links["activate"]
+
+        # Request activation of the 'visual' asset:
+        activate_result = requests.get(activation_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
+        # Parse out useful links
+        links = result.json()[u"visual"]["_links"]
+        self_link = links["_self"]
+        activation_link = links["activate"]
+
+        # Request activation of the 'visual' asset:
+        activate_result = requests.get(activation_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
+        activation_status_result = requests.get(self_link, auth=HTTPBasicAuth(PLANET_API_KEY, ''))
+
+        print(activation_status_result.json()["status"])
+
+        # Image can be downloaded by making a GET with your Planet API key, from here:
+        download_link = activation_status_result.json()["location"]
+
+        _, local_filename = mkstemp(dir=".", suffix='.tif')
+        r = requests.get(download_link, stream=True, verify=False)
+        with open(local_filename, 'wb') as fp:
+            shutil.copyfileobj(r.raw, fp)
+
+        print(download_link)
