4959baca102bd9fe07c0f66ab054cbc36cae8719
nilshempelmann <info@nilshempelmann.de>
2017-12-07 14:28:15 +0100

    merge par jour

--- scripts/merge_sats.py
+++ scripts/merge_sats.py
@@ -1,6 +1,6 @@
 from flyingpigeon import gdal_merge as gm
 from os import listdir
-from os.path import join
+from os.path import join, basename
 from tempfile import mkstemp
 import sys
 
@@ -21,34 +21,42 @@ import matplotlib.pyplot as plt
 #
 # % gdal_merge.py -init "0 0 255" -o out.tif in1.tif in2.tif
 
+DIR = '/home/nils/birdhouse/var/lib/pywps/cache/flyingpigeon/EO_data/PSScene4Band/analytic'
+# DIR = '/home/nils/data/planet/PSScene3Band/'
+pics = [join(DIR, pic) for pic in listdir(DIR) if '.tif' in pic]
 
-DIR = '/home/nils/data/planet/PSScene3Band/'
-pics = [join(DIR, pic) for pic in listdir(DIR)]
+dates = set()
+dates = dates.union([basename(pic).split('_')[0] for pic in pics])
+for date in dates:
+    print date
 
-_, filename = mkstemp(dir='/home/nils/data/planet/', suffix='.tif')
+    _, filename = mkstemp(dir='/home/nils/data/planet/', suffix='.tif')
 
-call = ['-o',  filename]
-for pic in pics:
-    call.extend([pic])
+    call = ['-o',  filename]
+    pics_day = [p for p in pics if date in p]
+    for pic in pics_day:
+        call.extend([pic])
 
-sys.argv[1:] = call  # ['-o',  outfile, pics[0], pics[1], pics[2], pics[3], pics[4]]
+    sys.argv[1:] = call  # ['-o',  outfile, pics[0], pics[1], pics[2], pics[3], pics[4]]
 
-gm.main()
+    gm.main()
 
-filename
+    filename
 
-cube = gdal.Open(filename)
-bnd1 = cube.GetRasterBand(1)
-bnd2 = cube.GetRasterBand(2)
-bnd3 = cube.GetRasterBand(3)
+    cube = gdal.Open(filename)
+    bnd1 = cube.GetRasterBand(1)
+    bnd2 = cube.GetRasterBand(2)
+    bnd3 = cube.GetRasterBand(3)
 
-img1 = bnd1.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
-img2 = bnd2.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
-img3 = bnd3.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
+    img1 = bnd1.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
+    img2 = bnd2.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
+    img3 = bnd3.ReadAsArray(0, 0, cube.RasterXSize, cube.RasterYSize)
 
-img = np.dstack((img1, img2, img3))
+    img = np.dstack((img1, img2, img3))
 
-f = plt.figure(figsize=(20, 10), dpi=600, facecolor='w', edgecolor='k', bbox='tight')
-plt.imshow(img)
+    f = plt.figure(figsize=(20, 10), dpi=600, facecolor='w', edgecolor='k') # , bbox='tight'
+    plt.imshow(img)
 
-plt.show()
+    _, picname = mkstemp(dir='/home/nils/data/planet/', suffix='.png')
+    plt.savefig(picname)
+    plt.show()
