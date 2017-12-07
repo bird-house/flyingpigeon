6a77119f27d366114ee3ed606732a511fcbb88a1
nilshempelmann <info@nilshempelmann.de>
2017-12-07 19:00:21 +0100

    cartopy plot geotif

--- scripts/eo_plotimage.py
+++ scripts/eo_plotimage.py
@@ -30,3 +30,46 @@ plt.imshow(img3)
 _, picname = mkstemp(dir='/home/nils/data/planet/', suffix='.tif')
 plt.savefig(picname)
 plt.show()
+
+
+**********
+# https://ocefpaf.github.io/python4oceanographers/blog/2015/03/02/geotiff/
+from osgeo import gdal, osr
+from tempfile import mkstemp
+
+gdal.UseExceptions()
+
+
+fname = '/home/nils/birdhouse/var/lib/pywps/cache/flyingpigeon/EO_data/PSScene3Band/visual/20171126_084935_1029.tif'
+
+ds = gdal.Open(fname)
+data = ds.ReadAsArray()
+gt = ds.GetGeoTransform()
+proj = ds.GetProjection()
+
+inproj = osr.SpatialReference()
+inproj.ImportFromWkt(proj)
+
+print(inproj)
+
+import cartopy.crs as ccrs
+
+projcs = inproj.GetAuthorityCode('PROJCS')
+projection = ccrs.epsg(projcs)
+print(projection)
+
+
+import matplotlib.pyplot as plt
+
+subplot_kw = dict(projection=projection)
+fig, ax = plt.subplots(figsize=(9, 9), subplot_kw=subplot_kw)
+
+extent = (gt[0], gt[0] + ds.RasterXSize * gt[1],
+          gt[3] + ds.RasterYSize * gt[5], gt[3])
+
+img = ax.imshow(data[:3, :, :].transpose((1, 2, 0)), extent=extent,
+                origin='upper')
+
+_, picname = mkstemp(dir='/home/nils/data/planet/', suffix='.tif')
+
+plt.savefig(picname)
