c215604328dd784d7ae2a21acf19f0832689e09a
nilshempelmann <info@nilshempelmann.de>
2017-12-12 15:56:57 +0100

    fetch scihub COPERNICUS

---
+++ flyingpigeon/processes/wps_EO_COP_fetch.py
@@ -0,0 +1,178 @@
+from pywps import Process
+# from pywps import LiteralInput
+from pywps import ComplexInput, LiteralInput, ComplexOutput
+from pywps import Format, FORMATS
+from pywps.app.Common import Metadata
+
+from flyingpigeon.log import init_process_logger
+from flyingpigeon.utils import rename_complexinputs
+
+from datetime import datetime as dt
+from datetime import timedelta, time
+
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
+
+class EO_COP_fetchProcess(Process):
+    """
+    TODO: like FetchProcess
+    """
+    def __init__(self):
+        inputs = [
+            LiteralInput("products", "Earth Observation Product Type",
+                         abstract="Choose Earth Observation Products",
+                         default='SLC',
+                         data_type='string',
+                         min_occurs=1,
+                         max_occurs=1,
+                         allowed_values=['SLC']
+                         ),
+
+            LiteralInput('BBox', 'Bounding Box',
+                         data_type='string',
+                         abstract="Enter a bbox: min_lon, max_lon, min_lat, max_lat."
+                                  " min_lon=Western longitude,"
+                                  " max_lon=Eastern longitude,"
+                                  " min_lat=Southern or northern latitude,"
+                                  " max_lat=Northern or southern latitude."
+                                  " For example: -80,50,20,70",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default='14,15,8,9',
+                         ),
+
+            LiteralInput('start', 'Start Date',
+                         data_type='date',
+                         abstract='First day of the period to be searched for EO data.'
+                                  '(if not set, 30 days befor end of period will be selected',
+                         default=(dt.now() - timedelta(days=30)).strftime('%Y-%m-%d'),
+                         min_occurs=0,
+                         max_occurs=1,
+                         ),
+
+            LiteralInput('end', 'End Date',
+                         data_type='date',
+                         abstract='Last day of the period to be searched for EO data.'
+                                  '(if not set, current day is set.)',
+                         default=dt.now().strftime('%Y-%m-%d'),
+                         min_occurs=0,
+                         max_occurs=1,
+                         ),
+
+            LiteralInput('unsername', 'User Name',
+                         data_type='string',
+                         abstract='Authentification user name for the COPERNICUS Sci-hub ',
+                         # default='2013-12-31',
+                         min_occurs=1,
+                         max_occurs=1,
+                         ),
+
+            LiteralInput('password', 'Password',
+                         data_type='string',
+                         abstract='Authentification password for the COPERNICUS Sci-hub ',
+                         # default='2013-12-31',
+                         min_occurs=1,
+                         max_occurs=1,
+                         ),
+        ]
+
+        outputs = [
+            ComplexOutput("output", "Fetched Files",
+                          abstract="File containing the local pathes to downloades files.",
+                          supported_formats=[Format('text/plain')],
+                          as_reference=True,
+                          ),
+
+            ComplexOutput("output_log", "Logging information",
+                          abstract="Collected logs during process run.",
+                          supported_formats=[Format("text/plain")],
+                          as_reference=True,
+                          )
+        ]
+
+        super(FetcheodataProcess, self).__init__(
+            self._handler,
+            identifier="EO_fetch",
+            title="Earth Observation Fetch Resources",
+            version="0.1",
+            abstract="Fetch EO Data to the local file"
+                     "system of the birdhouse compute provider.",
+            metadata=[
+                Metadata('Documentation', 'http://flyingpigeon.readthedocs.io/en/latest/'),
+            ],
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
+        )
+
+    def _handler(self, request, response):
+        response.update_status("start fetching resource", 10)
+
+        init_process_logger('log.txt')
+        response.outputs['output_log'].file = 'log.txt'
+
+        products = [inpt.data for inpt in request.inputs['products']]
+
+        bbox = []  # order xmin ymin xmax ymax
+        bboxStr = request.inputs['BBox'][0].data
+        bboxStr = bboxStr.split(',')
+        bbox.append(float(bboxStr[0]))
+        bbox.append(float(bboxStr[2]))
+        bbox.append(float(bboxStr[1]))
+        bbox.append(float(bboxStr[3]))
+
+        if 'end' in request.inputs:
+            end = request.inputs['end'][0].data
+            end = dt.combine(end, time(23, 59, 59))
+        else:
+            end = dt.now()
+
+        if 'start' in request.inputs:
+            start = request.inputs['start'][0].data
+            start = dt.combine(start, time(0, 0, 0))
+        else:
+            start = end - timedelta(days=30)
+
+        if (start > end):
+            start = dt.now() - timedelta(days=30)
+            end = dt.now()
+            LOGGER.exception("periode end befor periode start, period is set to the last 30 days from now")
+
+        unsername = request.inputs['unsername'][0].data
+        password = request.inputs['password'][0].data
+
+        api = SentinelAPI(username, password)
+
+        products = api.query(footprint,
+                             date=(start, end),
+                             producttype='SLC',
+                             orbitdirection='ASCENDING')
+        api.download_all(products)
+
+        # _, filepathes = mkstemp(dir='.', suffix='.txt')
+        # try:
+        #     with open(filepathes, 'w') as fp:
+        #         fp.write('######################################################\n')
+        #         fp.write('### Following files are stored to compute provider ###:\n')
+        #         fp.write('######################################################\n')
+        #         fp.write('\n')
+        #         for f in resources:
+        #             fp.write('%s \n' % os.path.realpath(f))
+        #         fp.write('/n')
+        #         fp.write('/n')
+        #         fp.write('######################################################\n')
+        #         fp.write('### Following files didn\'t want to wake up       ###:\n')
+        #         fp.write('######################################################\n')
+        #         for f in resources_sleeping:
+        #             fp.write('%s \n' % f)
+        #     response.outputs['output'].file = filepathes
+        # except:
+        #     LOGGER.exception('failed to write resources to textfile')
+        # response.outputs['output'].file = write_fileinfo(resource, filepath=True)
+
+        response.update_status("done", 100)
+        return response
--- scripts/eo_fetch_sentinel_geojson.py
+++ scripts/eo_fetch_sentinel_geojson.py
@@ -10,39 +10,120 @@ from flyingpigeon import config
 
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 
+api = SentinelAPI('nilshempelmann', '*****')
 
-api = SentinelAPI('info@nilshempelmann.de', '*****')   0Zd0If0ed2G
+geom = {
+  "type": "Polygon",
+  "coordinates": [
+          [
+            [
+              14.00,
+              8.00
+            ],
+            [
+              16.00,
+              8.00
+            ],
+            [
+              16.00,
+              10.00
+            ],
+            [
+              14.00,
+              10.00
+            ],
+            [
+              14.00,
+              8.00
+            ]
+          ]
+        ]
+}
 
-geom = {"type": "Polygon", "coordinates": [[[-69.87682044199994, 12.427394924000097], [-70.05809485599988, 12.537176825000088], [-70.04873613199993, 12.632147528000104], [-69.93639075399994, 12.53172435100005], [-69.87682044199994, 12.427394924000097]]]}
+#
+geom = {
+  "type": "Polygon",
+  "coordinates": [
+          [
+            [
+              14.600830078125,
+              8.677421123289992
+            ],
+            [
+              14.797210693359375,
+              8.677421123289992
+            ],
+            [
+              14.797210693359375,
+              8.90678000752024
+            ],
+            [
+              14.600830078125,
+              8.90678000752024
+            ],
+            [
+              14.600830078125,
+              8.677421123289992
+            ]
+          ]
+        ]
+}
 
-footprint = geojson_to_wkt(read_geojson('search_polygon.geojson'))
-products = api.query(footprint,
-                     producttype='SLC',
-                     orbitdirection='ASCENDING')
-api.download_all(products)
 
-/home/nils/data/transfrontalier/transfrontalier.shp
-gci = GeomCabinetIterator(path=join(sh_path, 'countries.shp'))
-
-for row in gci:
-    gjson = mapping(row['geom'])
-    dump = json.dumps(gjson)
-    print(dump)
-    break
-
-
-        env.DIR_SHPCABINET = config.shapefiles_path()
-        sc_iter = ShpCabinetIterator(geom)
-        result = []
-
-        if geom == 'countries':
-            for row in sc_iter:
-                for polygon in polygons:
-                    if row['properties']['ADM0_A3'] == polygon:
-                        result.append(row['properties']['UGID'])
-
-
-api = SentinelAPI('user', 'password')
-geom = {"type": "Polygon", "coordinates": [[[-69.87682044199994, 12.427394924000097], [-70.05809485599988, 12.537176825000088], [-70.04873613199993, 12.632147528000104], [-69.93639075399994, 12.53172435100005], [-69.87682044199994, 12.427394924000097]]]}
+# geom = {"type": "Polygon", "coordinates": [[[-69.87682044199994, 12.427394924000097], [-70.05809485599988, 12.537176825000088], [-70.04873613199993, 12.632147528000104], [-69.93639075399994, 12.53172435100005], [-69.87682044199994, 12.427394924000097]]]}
+# footprint = geojson_to_wkt(read_geojson('search_polygon.geojson'))
 
 footprint = geojson_to_wkt(geom)
+
+from datetime import datetime as dt
+from datetime import timedelta
+end = dt.now()
+start = dt.now() - timedelta(days=30)
+period = (start, end)
+
+
+products = api.query(footprint,
+                     date = (start, end),
+                     platformname = 'Sentinel-2',
+                     cloudcoverpercentage = (0, 30))
+for key in products.keys()
+    products[key]
+    api.get_product_odata(key, full=True)
+    api.download(key, directory_path='/home/nils/data/')
+
+
+
+# products = api.query(footprint,
+#                      date=period,
+#                      # producttype='SLC',
+#                      orbitdirection='ASCENDING',
+#                      platformname='Sentinel-2')
+#
+# api.download_all(products)
+
+#
+# /home/nils/data/transfrontalier/transfrontalier.shp
+# gci = GeomCabinetIterator(path=join(sh_path, 'countries.shp'))
+#
+# for row in gci:
+#     gjson = mapping(row['geom'])
+#     dump = json.dumps(gjson)
+#     print(dump)
+#     break
+#
+#
+#         env.DIR_SHPCABINET = config.shapefiles_path()
+#         sc_iter = ShpCabinetIterator(geom)
+#         result = []
+#
+#         if geom == 'countries':
+#             for row in sc_iter:
+#                 for polygon in polygons:
+#                     if row['properties']['ADM0_A3'] == polygon:
+#                         result.append(row['properties']['UGID'])
+#
+#
+# api = SentinelAPI('user', 'password')
+# geom = {"type": "Polygon", "coordinates": [[[-69.87682044199994, 12.427394924000097], [-70.05809485599988, 12.537176825000088], [-70.04873613199993, 12.632147528000104], [-69.93639075399994, 12.53172435100005], [-69.87682044199994, 12.427394924000097]]]}
+#
+# footprint = geojson_to_wkt(geom)
