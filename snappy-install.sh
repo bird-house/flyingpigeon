b743c837fa061e80402478d6eb4c437cfe10deba
nilshempelmann <info@nilshempelmann.de>
2018-01-22 19:03:03 +0100

    run snappy installer

--- snappy-install.sh
+++ snappy-install.sh
@@ -1,9 +1,6 @@
-
-
 [ -f esa-snap_sentinel_unix_6_0.sh ] && echo "esa snap installation file allready downloaded " || wget http://step.esa.int/downloads/6.0/installers/esa-snap_sentinel_unix_6_0.sh
 
 cat <<EOT >> response.varfile
-# install4j response file for ESA SNAP 6.0
 deleteSnapDir=ALL
 executeLauncherWithPythonAction$Boolean=true
 forcePython$Boolean=true
@@ -14,14 +11,23 @@ sys.component.S1TBX$Boolean=true
 sys.component.S2TBX$Boolean=true
 sys.component.S3TBX$Boolean=true
 sys.component.SNAP$Boolean=true
-sys.installationDir=$HOME/birdhouse/
+sys.installationDir=$HOME/birdhouse/snap
 sys.languageId=en
 sys.programGroupDisabled$Boolean=false
 sys.symlinkDir=/usr/local/bin
 EOT
 
-bash ./esa-snap_sentinel_unix_6_0.sh -q -varfile response.varfile -dir $HOME/birdhouse/
+bash ./esa-snap_sentinel_unix_6_0.sh -q -varfile response.varfile
 
-cp -r $HOME/birdhouse/.snap/snap-python/snappy $HOME/.conda/envs/flyingpigeon/lib/python2.7/site-packages/
+PY=$HOME/.conda/envs/flyingpigeon/bin/python
+$HOME/birdhouse/snap/bin/snappy-conf $PY $HOME/birdhouse/snap/snap-python/
+
+cp -r $HOME//birdhouse/snap/snap-python/snappy $HOME/.conda/envs/flyingpigeon/lib/python2.7/site-packages/
+
+echo 'snappy cpoied to site-packages'
 
 rm response.varfile
+
+echo '***************************************************'
+echo '*********** snappy installation done **************'
+echo '***************************************************'
