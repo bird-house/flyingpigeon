2df575ce2a08b5ba8192c0ff21b84df749fb6368
nilshempelmann <info@nilshempelmann.de>
2018-01-23 09:05:55 +0100

    small change

--- snappy-install.sh
+++ snappy-install.sh
@@ -1,3 +1,8 @@
+echo '***************************************************'
+echo '*********** start snappy installation *************'
+echo '***************************************************'
+
+
 [ -f esa-snap_sentinel_unix_6_0.sh ] && echo "esa snap installation file allready downloaded " || wget http://step.esa.int/downloads/6.0/installers/esa-snap_sentinel_unix_6_0.sh
 
 cat <<EOT >> response.varfile
@@ -24,7 +29,7 @@ $HOME/birdhouse/snap/bin/snappy-conf $PY $HOME/birdhouse/snap/snap-python/
 
 cp -r $HOME//birdhouse/snap/snap-python/snappy $HOME/.conda/envs/flyingpigeon/lib/python2.7/site-packages/
 
-echo 'snappy cpoied to site-packages'
+echo 'snappy copied to site-packages'
 
 rm response.varfile
 
