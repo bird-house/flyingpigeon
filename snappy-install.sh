2c06f5c393e12ea8e1bf09d6c5b5661fa99e156c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2018-01-24 15:43:54 +0100

    preprared snappy installer for post-install

--- snappy-install.sh
+++ snappy-install.sh
@@ -1,37 +1,57 @@
+# configuration
+
+CWD=$(pwd -P)
+
+# These paths can be set by Makefile in the post-install target
+DOWNLOAD_CACHE="$CWD/downloads"
+PREFIX="$CWD/parts"
+CONDA_ENV="$HOME/.conda/envs/flyingpigeon"
+
+# misc
+ESA_SNAP="esa-snap_sentinel_unix_6_0.sh"
+PYTHON=$CONDA_ENV/bin/python
+INSTALL_DIR=$PREFIX/snap
+VARFILE=$DOWNLOAD_CACHE/response.varfile
+
+# end of configuration
+
+
+
 echo '***************************************************'
 echo '*********** start snappy installation *************'
 echo '***************************************************'
 
+mkdir -p $INSTALL_DIR
 
-[ -f esa-snap_sentinel_unix_6_0.sh ] && echo "esa snap installation file allready downloaded " || wget http://step.esa.int/downloads/6.0/installers/esa-snap_sentinel_unix_6_0.sh
+[ -f "$DOWNLOAD_CACHE/$ESA_SNAP" ] && echo "esa snap installation file allready downloaded " || wget -P $DOWNLOAD_CACHE http://step.esa.int/downloads/6.0/installers/$ESA_SNAP
 
-cat <<EOT >> response.varfile
+cat <<EOT >> $VARFILE
 deleteSnapDir=ALL
 executeLauncherWithPythonAction$Boolean=true
 forcePython$Boolean=true
-pythonExecutable=$HOME.conda/envs/flyingpigeon/bin/python
+pythonExecutable=$PYTHON
 sys.adminRights$Boolean=false
 sys.component.RSTB$Boolean=true
 sys.component.S1TBX$Boolean=true
 sys.component.S2TBX$Boolean=true
 sys.component.S3TBX$Boolean=true
 sys.component.SNAP$Boolean=true
-sys.installationDir=$HOME/birdhouse/snap
+sys.installationDir=$INSTALL_DIR
 sys.languageId=en
 sys.programGroupDisabled$Boolean=false
 sys.symlinkDir=/usr/local/bin
 EOT
 
-bash ./esa-snap_sentinel_unix_6_0.sh -q -varfile response.varfile
+bash $DOWNLOAD_CACHE/$ESA_SNAP -q -varfile $VARFILE
 
-PY=$HOME/.conda/envs/flyingpigeon/bin/python
-$HOME/birdhouse/snap/bin/snappy-conf $PY $HOME/birdhouse/snap/snap-python/
+PY=$PYTHON
+$INSTALL_DIR/bin/snappy-conf $PY $INSTALL_DIR/snap-python/
 
-cp -r $HOME//birdhouse/snap/snap-python/snappy $HOME/.conda/envs/flyingpigeon/lib/python2.7/site-packages/
+cp -r $INSTALL_DIR/snap-python/snappy $CONDA_ENV/lib/python2.7/site-packages/
 
 echo 'snappy copied to site-packages'
 
-rm response.varfile
+rm $VARFILE
 
 echo '***************************************************'
 echo '*********** snappy installation done **************'
