b9a97e12f000a98b800902e5ebf359296c15db34
nilshempelmann <info@nilshempelmann.de>
2018-01-24 15:15:23 +0100

    include snappy installation in makefile

--- requirements/snappy-install.sh
+++ requirements/snappy-install.sh
@@ -2,21 +2,28 @@ echo '***************************************************'
 echo '*********** start snappy installation *************'
 echo '***************************************************'
 
+CONDA_ENV=$1
+PY=$(which python)
+APP_ROOT=$2
+installationDir=$APP_ROOT'/../snap'
+
+echo 'snappy installing into:' $CONDA_ENV
 
 [ -f esa-snap_sentinel_unix_6_0.sh ] && echo "esa snap installation file allready downloaded " || wget http://step.esa.int/downloads/6.0/installers/esa-snap_sentinel_unix_6_0.sh
+
 
 cat <<EOT >> response.varfile
 deleteSnapDir=ALL
 executeLauncherWithPythonAction$Boolean=true
 forcePython$Boolean=true
-pythonExecutable=$HOME.conda/envs/flyingpigeon/bin/python
+pythonExecutable=$PY
 sys.adminRights$Boolean=false
 sys.component.RSTB$Boolean=true
 sys.component.S1TBX$Boolean=true
 sys.component.S2TBX$Boolean=true
 sys.component.S3TBX$Boolean=true
 sys.component.SNAP$Boolean=true
-sys.installationDir=$HOME/birdhouse/snap
+sys.installationDir=$installationDir
 sys.languageId=en
 sys.programGroupDisabled$Boolean=false
 sys.symlinkDir=/usr/local/bin
@@ -24,10 +31,9 @@ EOT
 
 bash ./esa-snap_sentinel_unix_6_0.sh -q -varfile response.varfile
 
-PY=$HOME/.conda/envs/flyingpigeon/bin/python
-$HOME/birdhouse/snap/bin/snappy-conf $PY $HOME/birdhouse/snap/snap-python/
+$installationDir'/bin/snappy-conf' $PY $installationDir'/snap-python/'
 
-cp -r $HOME//birdhouse/snap/snap-python/snappy $HOME/.conda/envs/flyingpigeon/lib/python2.7/site-packages/
+cp -r $installationDir'/snap-python/snappy' $CONDA_ENV'/flyingpigeon/lib/python2.7/site-packages/'
 
 echo 'snappy copied to site-packages'
 
