c1b30c3f4ebcc227c871006d89c387144eaf5e0b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2018-03-02 17:21:08 +0100

    skip snappy tests if not available

--- flyingpigeon/tests/test_snap.py
+++ flyingpigeon/tests/test_snap.py
@@ -1,5 +1,9 @@
 import pytest
 
+# run tests only if snappy is available
+snappy = pytest.importorskip("snappy")
+
+
 def test_snappy():
     import snappy
     from snappy import (ProductIO, ProductUtils, ProgressMonitor, jpy)
