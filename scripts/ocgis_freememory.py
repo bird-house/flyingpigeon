647019ee2b59c01b43856d605318f74528e9ce06
nilshempelmann <info@nilshempelmann.de>
2018-06-23 15:20:13 +0200

    update freememory check

--- scripts/ocgis_freememory.py
+++ scripts/ocgis_freememory.py
@@ -1,4 +1,5 @@
-from flyingpigeon import utils
+from flyingpigeon import datafetch
+ncs = datafetch.reanalyses(start=2000, end=2017)
 
 from ocgis import RequestDataset, OcgOperations, env
 from ocgis.util.large_array import compute
@@ -7,24 +8,28 @@ from datetime import datetime as dt
 import uuid
 
 # years = range(1948,2018)
-years = range(2007, 2017)
 
-ncs = []
-for year in years:
-    url = 'https://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/pressure/slp.%s.nc' % (year)
-    ncs.extend(utils.download(url, cache=True))
+# ncs = []
+# for year in years:
+#     url = 'https://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/pressure/slp.%s.nc' % (year)
+#     ncs.extend([utils.download_file(url)])
 
-
+# print ncs
 level_range = [700, 700]
 time_range = [dt.strptime('20100315', '%Y%m%d'), dt.strptime('20111210', '%Y%m%d')]
 bbox = [-80, 20, 20, 70]
+
+# TODO: BUG: ocg compute is not running if calc == None
+calc = '%s=%s*1' % (variable, variable)
+#
 
 
 rd = RequestDataset(ncs)
 
 ops = OcgOperations(rd,
                     time_range=time_range,
-                    level_range=level_range,
+                    calc = '%s=%s*1' % ('slp', 'slp'),
+                    # level_range=level_range,
                     geom=bbox,
                     output_format='nc',
                     prefix='ocgis_module_optimisation',
@@ -32,34 +37,42 @@ ops = OcgOperations(rd,
 
 # ###################################
 # check free memory available somehow
-
-# free_memory = ...
+from eggshell import util_functions as ufs
+free_memory = ufs.FreeMemory(unit='MB')
 
 # ###########################
 # check required memory space
-# requ_memory
 
-# copmpare
-# if requ_memory < free_memory/2:
+data_kb = ops.get_base_request_size()['total']
+data_mb = data_kb / 1024.
 
-shnip = dt.now()
+# ###########################
+# check if half of the available memory can take the required data load
 
-geom = ops.execute()
+if data_mb < fm.user_free/2:
+    print "enough memory. data can be processed directly"
+    shnip = dt.now()
 
-shnap = dt.now()
+    geom = ops.execute()
 
-print 'operation performed with execute in %s sec' % (shnap - shnip).total_seconds()
-print geom
+    shnap = dt.now()
 
-# else
+    print 'operation performed with execute in %s sec' % (shnap - shnip).total_seconds()
+    print geom
 
 ########################
-# calculate tile dimension:
-tile_dimension = 5  # yea nice calculation
+# simulation if memory is not enough for the dataload. Than calculate in chunks
 
-shnip = dt.now()
-geom = compute(ops, tile_dimension=tile_dimension, verbose=True)
-shnap = dt.now()
+fm_sim = data_mb/2
 
-print 'operation performed with compute in  %s sec' % (shnip - shnap).total_seconds()
-print geom
+if data_mb >= fm_sim:
+    print "NOT enough memory. data will be processed in chunks"
+    # calculate tile dimension:
+    tile_dimension= 10 # TODO: needs to be calculated based on dataload and available memory
+
+    shnip = dt.now()
+    geom = compute(ops, tile_dimension=tile_dimension, verbose=True)
+    shnap = dt.now()
+
+    print 'operation performed with compute in  %s sec' % (shnap - shnip ).total_seconds()
+    print geom
