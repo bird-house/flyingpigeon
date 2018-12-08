b400046ae4629d52fcad0b0876bfb048f8eb1935
nilshempelmann <info@nilshempelmann.de>
2018-12-08 11:56:37 +0100

    update common.py

--- tests/common.py
+++ tests/common.py
@@ -6,6 +6,57 @@ VERSION = "1.0.0"
 WPS, OWS = get_ElementMakerForVersion(VERSION)
 xpath_ns = get_xpath_ns(VERSION)
 
+TESTS_HOME = os.path.abspath(os.path.dirname(__file__))
+CFG_FILE = os.path.join(TESTS_HOME, 'test.cfg')
+
+TESTDATA = {
+    'cmip5_tasmax_2006_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip5',
+        'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200601-200612.nc')),
+    'cmip5_tasmax_2007_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip5',
+        'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200701-200712.nc')),
+    'cmip3_tas_sresb1_da_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip3',
+        'tas.sresb1.giss_model_e_r.run1.atm.da.nc')),
+    'cmip3_tas_sresa2_da_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip3',
+        'tas.sresa2.miub_echo_g.run1.atm.da.nc')),
+    'cmip3_tasmin_sresa2_da_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip3',
+        'tasmin.sresa2.miub_echo_g.run1.atm.da.nc')),
+    'cmip3_tasmax_sresa2_da_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip3',
+        'tasmax.sresa2.miub_echo_g.run1.atm.da.nc')),
+    'cmip3_pr_sresa2_da_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cmip3',
+        'pr.sresa2.miub_echo_g.run1.atm.da.nc')),
+    'cordex_tasmax_2006_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cordex',
+        'tasmax_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon_200602-200612.nc')),
+    'cordex_tasmax_2007_nc': "file://{0}".format(os.path.join(
+        TESTS_HOME,
+        'testdata',
+        'cordex',
+        'tasmax_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon_200701-200712.nc')),
+}
+
 
 class WpsTestClient(WpsClient):
 
