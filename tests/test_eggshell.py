5d6af2b0ba81824761c70f8fc0d66ed0b5e55d5e
nilshempelmann <info@nilshempelmann.de>
2018-12-08 17:35:33 +0100

    eggshell tests passed

--- tests/test_eggshell.py
+++ tests/test_eggshell.py
@@ -2,26 +2,36 @@ import pytest
 
 from .common import TESTDATA
 
-from eggshell.nc import ocg_utils
-
-
-import os.path
+from os.path import basename, join
 import tempfile
 import tarfile
 import zipfile
 from netCDF4 import Dataset
 
 from eggshell import utils
-# from eggshell.utils import local_path
+from eggshell.utils import local_path
+from eggshell.nc import ocg_utils
+from eggshell.nc import nc_utils
 from eggshell.config import Paths
 import flyingpigeon as fp
 paths = Paths(fp)
 
 
+def test_Paths():
+    assert "flyingpigeon/tests/testdata" in paths.testdata
+    assert 'flyingpigeon/data' in paths.data
+    assert 'flyingpigeon/data/shapefiles' in paths.shapefiles
+
+
+def test_local_path():
+    assert local_path('file:///tmp/test.nc') == '/tmp/test.nc'
+    assert local_path('/tmp/test.nc') == '/tmp/test.nc'
+
+
 def test_ocgis_import():
     from ocgis import constants
 
-#
+
 # def test_has_Lambert_Conformal():
 #     has_lambert = ocg_utils.has_Lambert_Conformal(
 #         [local_path(TESTDATA['cordex_tasmax_2006_nc']),
@@ -33,60 +43,55 @@ def test_gdal():
     from flyingpigeon.subset import clipping
 
 
-def test_local_path():
-    assert local_path('file:///tmp/test.nc') == '/tmp/test.nc'
-    assert local_path('/tmp/test.nc') == '/tmp/test.nc'
-
-
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_download_with_cache():
     filename = utils.download(TESTDATA['cmip5_tasmax_2006_nc'], cache=paths.cache)
-    assert os.path.basename(filename) == 'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200601-200612.nc'
+    assert basename(filename) == 'tasmax_Amon_MPI-ESM-MR_rcp45_r1i1p1_200601-200612.nc'
 
 
 def test_archive_tar():
-    result = utils.archive(
-        [local_path(TESTDATA['cmip5_tasmax_2007_nc'])],
-        format='tar',
-        dir_output=tempfile.mkdtemp())
+    result = utils.archive(local_path(TESTDATA['cmip5_tasmax_2006_nc']),
+                           format='tar',
+                           output_dir=tempfile.mkdtemp())
     tar = tarfile.open(result)
     assert len(tar.getnames()) == 1
 
+#  [local_path(TESTDATA['cmip5_tasmax_2007_nc'])],
+
 
 def test_archive_zip():
-    result = utils.archive(
-        [local_path(TESTDATA['cmip5_tasmax_2007_nc'])],
+    result = utils.archive(local_path(TESTDATA['cmip5_tasmax_2006_nc']),
         format='zip',
-        dir_output=tempfile.mkdtemp())
+        output_dir=tempfile.mkdtemp())
     zipf = zipfile.ZipFile(result)
     assert len(zipf.namelist()) == 1
 
 
 def test_get_frequency():
-    freq = utils.get_frequency(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    freq = nc_utils.get_frequency(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 'mon' == freq
 
 
 def test_get_values():
-    values = utils.get_values(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    values = nc_utils.get_values(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 12 == len(values)
 
-    values = utils.get_values(local_path(TESTDATA['cordex_tasmax_2007_nc']))
+    values = nc_utils.get_values(local_path(TESTDATA['cordex_tasmax_2007_nc']))
     assert 12 == len(values)
 
-    values = utils.get_values([local_path(TESTDATA['cordex_tasmax_2006_nc']),
+    values = nc_utils.get_values([local_path(TESTDATA['cordex_tasmax_2006_nc']),
                                local_path(TESTDATA['cordex_tasmax_2007_nc'])])
     assert 23 == len(values)
 
 
 def test_get_time():
-    timestamps = utils.get_time(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    timestamps = nc_utils.get_time(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 12 == len(timestamps)
 
-    timestamps = utils.get_time(local_path(TESTDATA['cordex_tasmax_2007_nc']))
+    timestamps = nc_utils.get_time(local_path(TESTDATA['cordex_tasmax_2007_nc']))
     assert 12 == len(timestamps)
 
-    values = utils.get_values([local_path(TESTDATA['cordex_tasmax_2006_nc']),
+    values = nc_utils.get_values([local_path(TESTDATA['cordex_tasmax_2006_nc']),
                                local_path(TESTDATA['cordex_tasmax_2007_nc'])])
     assert 23 == len(values)
 
@@ -95,18 +100,18 @@ def test_get_time():
 def test_unrotate_pole():
     ncs = [local_path(TESTDATA['cordex_tasmax_2006_nc']),
            local_path(TESTDATA['cordex_tasmax_2007_nc'])]
-    lats, lons = utils.unrotate_pole(ncs)
+    lats, lons = nc_utils.unrotate_pole(ncs)
     assert lats.shape == (103, 106)
 
 
 def test_get_index_lat():
     ncs = [local_path(TESTDATA['cordex_tasmax_2006_nc']),
            local_path(TESTDATA['cordex_tasmax_2007_nc'])]
-    index = utils.get_index_lat(ncs)
+    index = nc_utils.get_index_lat(ncs)
     assert 1 == index
-    index = utils.get_index_lat(ncs[0])
+    index = nc_utils.get_index_lat(ncs[0])
     assert 1 == index
-    index = utils.get_index_lat(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    index = nc_utils.get_index_lat(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 1 == index
 
 
@@ -114,44 +119,44 @@ def test_get_coordinates():
     ncs = [local_path(TESTDATA['cordex_tasmax_2006_nc']),
            local_path(TESTDATA['cordex_tasmax_2007_nc'])]
 
-    lats, lons = utils.get_coordinates(ncs, unrotate=False)
+    lats, lons = nc_utils.get_coordinates(ncs, unrotate=False)
 
     assert 1 == len(lats.shape)
 
-    lats, lons = utils.get_coordinates(ncs)
+    lats, lons = nc_utils.get_coordinates(ncs)
     assert 103 == len(lats)
     assert 106 == len(lons)
 
 
 def test_get_variable():
-    variable = utils.get_variable(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    variable = nc_utils.get_variable(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 'tasmax' == variable
-    variable = utils.get_variable(local_path(TESTDATA['cordex_tasmax_2007_nc']))
+    variable = nc_utils.get_variable(local_path(TESTDATA['cordex_tasmax_2007_nc']))
     assert 'tasmax' == variable
 
 
 def test_sort_by_time():
-    result = utils.sort_by_time([local_path(TESTDATA['cmip5_tasmax_2007_nc']),
+    result = nc_utils.sort_by_time([local_path(TESTDATA['cmip5_tasmax_2007_nc']),
                                  local_path(TESTDATA['cmip5_tasmax_2006_nc'])])
     assert '200601' in result[0]
     assert '200701' in result[1]
 
 # def test_get_timestamps():
-#     start,end = utils.get_timestamps(local_path(TESTDATA['cmip5_tasmax_2006_nc']))
+#     start,end = nc_utils.get_timestamps(local_path(TESTDATA['cmip5_tasmax_2006_nc']))
 #     assert "20060116" == start
 #     assert "20061216" == end
 
 
 def test_get_timerange():
-    start, end = utils.get_timerange(local_path(TESTDATA['cmip5_tasmax_2006_nc']))
+    start, end = nc_utils.get_timerange(local_path(TESTDATA['cmip5_tasmax_2006_nc']))
     assert "20060116" == start
     assert "20061216" == end
 
-    start, end = utils.get_timerange(local_path(TESTDATA['cordex_tasmax_2007_nc']))
+    start, end = nc_utils.get_timerange(local_path(TESTDATA['cordex_tasmax_2007_nc']))
     assert "20070116" == start
     assert "20071216" == end
 
-    start, end = utils.get_timerange([local_path(TESTDATA['cordex_tasmax_2006_nc']),
+    start, end = nc_utils.get_timerange([local_path(TESTDATA['cordex_tasmax_2006_nc']),
                                      local_path(TESTDATA['cordex_tasmax_2007_nc'])])
     assert "20060215" == start
     assert "20071216" == end
@@ -159,15 +164,15 @@ def test_get_timerange():
 
 def test_drs_filename():
     # cordex
-    filename = utils.drs_filename(local_path(TESTDATA['cordex_tasmax_2006_nc']), skip_timestamp=False)
+    filename = nc_utils.drs_filename(local_path(TESTDATA['cordex_tasmax_2006_nc']), skip_timestamp=False)
     assert filename == "tasmax_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon_20060215-20061216.nc"
 
     # cordex ... skip timestamp
-    filename = utils.drs_filename(local_path(TESTDATA['cordex_tasmax_2006_nc']), skip_timestamp=True)
+    filename = nc_utils.drs_filename(local_path(TESTDATA['cordex_tasmax_2006_nc']), skip_timestamp=True)
     assert filename == "tasmax_EUR-44_MPI-M-MPI-ESM-LR_rcp45_r1i1p1_MPI-CSC-REMO2009_v1_mon.nc"
 
     # cmip5
-    filename = utils.drs_filename(local_path(TESTDATA['cmip5_tasmax_2006_nc']), skip_timestamp=False)
+    filename = nc_utils.drs_filename(local_path(TESTDATA['cmip5_tasmax_2006_nc']), skip_timestamp=False)
     assert filename == "tasmax_MPI-ESM-MR_RCP4.5_r1i1p1_20060116-20061216.nc"
 
 
@@ -176,7 +181,7 @@ def test_aggregations():
     nc_files.append(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     nc_files.append(local_path(TESTDATA['cmip5_tasmax_2006_nc']))
 
-    aggs = utils.aggregations(nc_files)
+    aggs = nc_utils.aggregations(nc_files)
 
     assert len(aggs) == 1
     assert "tasmax_MPI-ESM-MR_RCP4.5_r1i1p1" in aggs
@@ -200,14 +205,14 @@ def test_aggregations():
 
 
 def test_has_variable():
-    assert utils.has_variable(
+    assert nc_utils.has_variable(
         local_path(TESTDATA['cmip5_tasmax_2006_nc']), 'tasmax') is True
 
 
 def test_calc_grouping():
-    assert utils.calc_grouping('year') == ['year']
-    assert utils.calc_grouping('month') == ['month']
-    assert utils.calc_grouping('sem') == [
+    assert ocg_utils.calc_grouping('year') == ['year']
+    assert ocg_utils.calc_grouping('month') == ['month']
+    assert ocg_utils.calc_grouping('sem') == [
         [12, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], 'unique']
 
     # check invalid value: should raise an exception
