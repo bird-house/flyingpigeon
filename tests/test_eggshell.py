b8fbc4dbac8d6b65d49370bb6f85014b35004a7f
nilshempelmann <info@nilshempelmann.de>
2018-12-11 18:14:06 +0100

    update test_eggshell

--- tests/test_eggshell.py
+++ tests/test_eggshell.py
@@ -129,9 +129,9 @@ def test_get_coordinates():
 
 
 def test_get_variable():
-    variable = nc_utils.get_variable(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
+    variable = ocg_utils.get_variable(local_path(TESTDATA['cmip5_tasmax_2007_nc']))
     assert 'tasmax' == variable
-    variable = nc_utils.get_variable(local_path(TESTDATA['cordex_tasmax_2007_nc']))
+    variable = ocg_utils.get_variable(local_path(TESTDATA['cordex_tasmax_2007_nc']))
     assert 'tasmax' == variable
 
 
@@ -204,8 +204,46 @@ def test_aggregations():
     assert agg['filename'] == 'tasmax_MPI-ESM-MR_RCP4.5_r1i1p1_20060116-20071216.nc'
 
 
+def get_coordinates(resource, variable=None, unrotate=False):
+    """
+    reads out the coordinates of a variable
+    :param resource: netCDF resource file
+    :param variable: variable name
+    :param unrotate: If True the coordinates will be returned for unrotated pole
+    :returns list, list: latitudes , longitudes
+    """
+    if type(resource) != list:
+        resource = [resource]
+
+    if variable is None:
+        variable = get_variable(resource)
+
+    if unrotate is False:
+        try:
+            if len(resource) > 1:
+                ds = MFDataset(resource)
+            else:
+                ds = Dataset(resource[0])
+
+            var = ds.variables[variable]
+            dims = list(var.dimensions)
+            if 'time' in dims: dims.remove('time')
+            # TODO: find position of lat and long in list and replace dims[0] dims[1]
+            lats = ds.variables[dims[0]][:]
+            lons = ds.variables[dims[1]][:]
+            ds.close()
+            LOGGER.info('got coordinates without pole rotation')
+        except Exception:
+            msg = 'failed to extract coordinates'
+            LOGGER.exception(msg)
+    else:
+        lats, lons = unrotate_pole(resource)
+        LOGGER.info('got coordinates with pole rotation')
+    return lats, lons
+
+
 def test_has_variable():
-    assert nc_utils.has_variable(
+    assert ocg_utils.has_variable(
         local_path(TESTDATA['cmip5_tasmax_2006_nc']), 'tasmax') is True
 
 
