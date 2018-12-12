df6fa6d113eb085a4db67ed8d4ff70eb2615364b
nilshempelmann <info@nilshempelmann.de>
2018-12-12 13:55:50 +0100

    update subset tests

--- tests/test_wps_subset_countries.py
+++ tests/test_wps_subset_countries.py
@@ -30,7 +30,8 @@ def test_wps_subset_countries():
     assert_response_success(resp)
 
     # Check output file size is smaller than input.
-    # out = get_output(resp.xml)
+    out = get_output(resp.xml)
+    assert 'output' in out.keys()
     # ins = os.path.getsize(TESTDATA['cmip5_tasmax_2006_nc'][6:])
     # outs = os.path.getsize(out['ncout'][6:])
     # assert (outs < ins)
