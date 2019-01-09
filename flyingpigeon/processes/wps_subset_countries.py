991d4d24380a0c908b740db8014f87c9048378cb
Pingu Carsti <ehbrecht@dkrz.de>
2019-01-09 19:22:38 +0100

    fix workdir

--- flyingpigeon/processes/wps_subset_countries.py
+++ flyingpigeon/processes/wps_subset_countries.py
@@ -6,7 +6,7 @@ from pywps.app.Common import Metadata
 from flyingpigeon.subset import clipping
 from flyingpigeon.subset import countries
 from eggshell.utils import archive, extract_archive
-from eggshell.utils import rename_complexinputs
+# from eggshell.utils import rename_complexinputs
 
 LOGGER = logging.getLogger("PYWPS")
 
@@ -86,8 +86,7 @@ class SubsetcountryProcess(Process):
             store_supported=True,
         )
 
-    @staticmethod
-    def _handler(request, response):
+    def _handler(self, request, response):
         # init_process_logger('log.txt')
         # response.outputs['output_log'].file = 'log.txt'
 
@@ -96,7 +95,8 @@ class SubsetcountryProcess(Process):
             request.inputs['resource'][0].url,
             request.inputs['resource'][0].data_format.mime_type))
         ncs = extract_archive(
-            resources=rename_complexinputs(request.inputs['resource']))
+            resources=[inpt.file for inpt in request.inputs['resource']],
+            output_dir=self.workdir)
         # mime_type=request.inputs['resource'][0].data_format.mime_type)
         # mosaic option
         # TODO: fix defaults in pywps 4.x
@@ -121,7 +121,7 @@ class SubsetcountryProcess(Process):
                 mosaic=mosaic,
                 spatial_wrapping='wrap',
                 # variable=variable,
-                # dir_output=os.path.abspath(os.curdir),
+                dir_output=self.workdir,
                 # dimension_map=dimension_map,
             )
             LOGGER.info('results %s' % results)
@@ -135,7 +135,7 @@ class SubsetcountryProcess(Process):
 
         # prepare tar file
         try:
-            tarf = archive(results)
+            tarf = archive(results, output_dir=self.workdir)
             LOGGER.info('Tar file prepared.')
         except Exception as ex:
             msg = 'Tar file preparation failed: {}'.format(str(ex))
