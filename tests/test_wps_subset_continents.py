3adef68a9adf1f5e6840d7a717d1392cec66d91d
nilshempelmann <info@nilshempelmann.de>
2020-05-10 16:35:17 +0200

    minor corrections

--- tests/test_wps_subset_continents.py
+++ tests/test_wps_subset_continents.py
@@ -6,30 +6,30 @@ from .common import TESTDATA, client_for, get_output, CFG_FILE
 
 
 datainputs_fmt = (
-    "resource=@xlink:href={0};"
+    "resource=files@xlink:href={0};"
     "region={1};"
 )
 
 
 def test_wps_subset_continents():
-    client = client_for(
-        Service(processes=[SubsetcontinentProcess()], cfgfiles=CFG_FILE))
+    client = client_for(Service(processes=[SubsetcontinentProcess()], cfgfiles=CFG_FILE))
 
     datainputs = datainputs_fmt.format(
         TESTDATA['cmip5_tasmax_2006_nc'],
         'Africa',
         )
 
+    # resp = client.get(
+    #     service='wps', request='execute', version='1.0.0',
+    #     identifier='subset_continents',
+    #     datainputs=datainputs)
+
     resp = client.get(
-        service='wps', request='execute', version='1.0.0',
-        identifier='subset_continents',
-        datainputs=datainputs)
+       "?service=WPS&request=Execute&version=1.0.0&identifier=subset_continents&datainputs={}".format(datainputs))
 
     assert_response_success(resp)
 
     # Check output file size is smaller than input.
     out = get_output(resp.xml)
     assert 'output' in out.keys()
-    # ins = os.path.getsize(TESTDATA['cmip5_tasmax_2006_nc'][6:])
-    # outs = os.path.getsize(out['ncout'][6:])
-    # assert (outs < ins)
+    assert 'metalink' in out
