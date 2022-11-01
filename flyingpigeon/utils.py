353beec9b1661f6784d380815ed7100e762485cb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 08:41:35 +0000

    Adding tarfile member sanitization to extractall()

--- flyingpigeon/utils.py
+++ flyingpigeon/utils.py
@@ -148,7 +148,26 @@ def extract_archive(resources, dir_output=None):
                 files.append(os.path.join(dir_output, arch))
             elif ext == 'tar':
                 with tarfile.open(arch, mode='r') as tar:
-                    tar.extractall()
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar)
                     files.extend([os.path.join(dir_output, f) for f in tar.getnames()])
             elif ext == 'zip':
                 with ZipFile(arch, mode='r') as zf:
